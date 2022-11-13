f6c282afec03f0d9fad28cdd6cd57e618b61609c
jacksonlmp <jacksonlmp17.jl@gmail.com>
2022-11-13 14:23:25 -0300

    Curso Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
             AlunosSeeder::class,
             ProfessoresSeeder::class,
             ServidorSeeder::class,
-            VinculosSeeder::class
+            VinculosSeeder::class,
+            CursoSeeder::class
         ]);
     }
 }
