4c55510e7d965219e499796bfd3e4283799fadd7
Camila Claudino <camila.claudino@upe.br>
2022-11-30 18:38:11 -0300

    Melhoria da tela editar servidor
    Foram feitas melhorias no modal de edição do servidor.

--- resources/views/servidores/components/modal_edit.blade.php
+++ resources/views/servidores/components/modal_edit.blade.php
@@ -1,11 +1,11 @@
 <div data-backdrop="static" data-keyboard="false" role="dialog" class="modal fade" id="modal_edit_{{$servidor->id}}" tabindex="-1" aria-hidden="true" >
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content modal-create">
+  <div class="modal-dialog modal-dialog-centered ">
+    <div class="modal-content modal-create" style="background: #EEEEEE;">
       <div class="modal-header" >
-        <h5 class="modal-title title">Editar servidor</h5>
+        <h5 class="modal-title title ">Editar servidor</h5>
         <button class="btn-close" data-bs-dismiss="modal" onClick="window.location.reload();" aria-label="Close"></button>
       </div>
-      <form action="{{route('servidor.update')}}" method="post">
+      <form class="p-3" action="{{route('servidor.update')}}" method="post">
         @csrf
         <input type="hidden" name="id" value="{{$servidor->id}}">
         <div class="modal-body">
@@ -13,7 +13,7 @@
             <div class="">
               <label for="nome_edit" class="form-label">Nome</label>
               <input name="name" type="text" placeholder="Digite o nome" value="{{old('name', $servidor->user->name)}}"
-              class="form-control input-modal-create @if(!empty($errors->update->first('name'))) is-invalid @endif">
+              class="form-control input-modal-create @if(!empty($errors->update->first('name'))) is-invalid @endif form-control-sm">
                 @if(!empty($errors->update->first('name')))
                   <span class="invalid-feedback d-block">
                     <strong> {{$errors->update->first('name')}} </strong>
@@ -25,7 +25,7 @@
             <div class="col-sm- 12 col-md-6 mb-3">
               <label for="cpf_edit" class="form-label">CPF</label>
               <input name="cpf" id="cpf_edit" type="text" placeholder="Digite o CPF" value="{{old('cpf', $servidor->cpf)}}"
-                class="form-control input-modal-create @if(!empty($errors->update->first('cpf'))) is-invalid @endif">
+                class="form-control input-modal-create @if(!empty($errors->update->first('cpf'))) is-invalid @endif form-control-sm">
                 @if(!empty($errors->update->first('cpf')))
                   <span class="invalid-feedback d-block">
                     <strong> {{$errors->update->first('cpf')}} </strong>
@@ -35,7 +35,7 @@
             <div class="col-sm- 12 col-md-6 mb-3">
               <label for="setor_edit" class="form-label">Setor</label>
               <input name="setor" type="text" placeholder="Digite o setor" value="{{old('setor', $servidor->setor)}}"
-              class="form-control input-modal-create @if(!empty($errors->update->first('setor'))) is-invalid @endif">
+              class="form-control input-modal-create @if(!empty($errors->update->first('setor'))) is-invalid @endif form-control-sm">
                 @if(!empty($errors->update->first('setor')))
                   <span class="invalid-feedback d-block">
                     <strong> {{$errors->update->first('setor')}} </strong>
@@ -47,7 +47,7 @@
             <div class="col-sm- 12 col-md-6 mb-3">
               <label for="email_edit" class="form-label">E-mail</label>
               <input name="email" type="text" placeholder="Digite o e-mail" value="{{old('email', $servidor->user->email)}}"
-              class="form-control input-modal-create @if(!empty($errors->update->first('email'))) is-invalid @endif">
+              class="form-control input-modal-create @if(!empty($errors->update->first('email'))) is-invalid @endif form-control-sm">
               @if(!empty($errors->update->first('email')))
                   <span class="invalid-feedback d-block">
                     <strong> {{$errors->update->first('email')}} </strong>
@@ -57,7 +57,7 @@
             <div class="col-sm- 12 col-md-6 mb-3">
               <label for="password_edit" class="form-label">Senha</label>
               <input name="password" type="password" placeholder="Digite a senha"
-              class="form-control input-modal-create @if(!empty($errors->update->first('password'))) is-invalid @endif">
+              class="form-control input-modal-create @if(!empty($errors->update->first('password'))) is-invalid @endif form-control-sm">
               @if(!empty($errors->update->first('password')))
                   <span class="invalid-feedback d-block">
                     <strong> {{$errors->update->first('password')}} </strong>
--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Fonte Roboto -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <!-- Scripts -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="css/projeto/app.css" rel="stylesheet" type="text/css" />
