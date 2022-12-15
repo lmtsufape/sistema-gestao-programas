403389639e13282ac91df62c7b3d197280b40a41
jacksonlmp <jacksonlmp17.jl@gmail.com>
2022-12-14 21:01:46 -0300

    Componentes de programa

---
+++ resources/views/Programa/components/modal_delete.blade.php
@@ -0,0 +1,23 @@
+<div class="modal fade" id="modal_delete_{{$programa->id}}" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 style="color: #131833; font-style: normal; font-weight: 600; font-size: 30px; line-height: 47px;">Confirmação</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <h5 style="color: #131833; font-style: normal; font-weight: 600; font-size: 20px; line-height: 47px;">
+          Deseja realmente deletar o programa {{$programa->nome}}?</h5>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+        <form action="{{url("/programas/>1")}}" method="post">
+          @method("DELETE")
+          @csrf
+          <input type="hidden" name="id" value="{{$programa->id}}">
+          <button type="submit" class="btn btn-danger">Confirmar exclusão</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
