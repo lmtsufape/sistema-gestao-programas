a46f924570bd3b8fc9fcec8b0d19f2829d8a35e2
Camila Claudino <camila.claudino@upe.br>
2022-12-22 15:28:49 -0300

    Criação da tela de listar servidor + modais
    Criação da tela de listar servidores junto com os modais de visualizar informações e de deletar.

--- resources/views/servidores/components/modal_show.blade.php
+++ resources/views/servidores/components/modal_show.blade.php
@@ -1,26 +1,39 @@
-<div class="modal fade" id="modal_show_{{$servidor->id}}" tabindex="-1" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content modal-create">
+<div class="modal fade " id="modal_show_{{$servidor->id}}" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered ">
+    <div class="modal-content modal-create" style="border-radius: 15px; background-color: #EEEEEE; font-family: 'Roboto', sans-serif;">
       <div class="modal-header" >
-        <h5 class="modal-title title">{{$servidor->user->name}}</h5>
+        <h5 class="modal-title title fw-bold">Informações</h5>
         <button class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
         <div class="modal-body">
-
-          <div class="row">
+          <div class="row pb-3">
             <div class="col-sm- 12 col-md-6 mb-3">
-              <label class="form-label">CPF</label>
-              <div class="modal-ver"> {{$servidor->cpf}}</div>
+              <label class="form-label"><strong>Nome</strong></label>
+              <div  style="background: #F5F5F5; padding:3px; height: 30px; box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.25); border-radius: 45px;"> {{$servidor->user->name}}</div>
+            </div> 
+            <div class="col-sm- 12 col-md-6 mb-3">
+              <label class="form-label"><strong>CPF</strong></label>
+              <div  style="background: #F5F5F5; padding:3px; height: 30px; box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.25); border-radius: 45px;"> {{$servidor->cpf}}</div>
             </div> 
 
             <div class="col-sm- 12 col-md-6 mb-3">
-              <label class="form-label">Tipo de servidor</label>
-              <div class="modal-ver"> {{$servidor->tipo_servidor}}</div>
+              <label class="form-label"><strong>Tipo de servidor</strong></label>
+              <div  style="background: #F5F5F5; padding:3px; height: 30px; box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.25); border-radius: 45px;"> {{$servidor->tipo_servidor}}</div>
             </div>
           </div>
-          <p></p>
-          <a class="btn btn-primary submit-button" data-bs-dismiss="modal" style="width: 200px" role="button">Voltar</a>
+             
         </div>
     </div>
   </div>
 </div>
+<style>
+  .btn-secondary{
+    color: #fff;
+    background-color: #2d3875;
+    border-color: #2d3875;
+  }
+  .btn-secondary:hover{
+    background-color: #4353ab;
+    border-color: #4353ab;
+  }
+</style>
--- resources/views/servidores/index.blade.php
+++ resources/views/servidores/index.blade.php
@@ -1,94 +1,196 @@
 @extends("templates.app")
-
 @section("body")
-  @can('servidor')
-  <div class="container">
+@can('servidor')
+<div class="container" style="font-family: 'Roboto', sans-serif;">
+  @if (session('sucesso'))
+  <div class="alert alert-danger">
+    {{session('sucesso')}}
+  </div>
+  @endif
+  <br>
+  <div style="margin-bottom: 10px;  gap: 20px; margin-top: 20px">
     <h1><strong>Servidores</strong></h1>
-    <a type="button" data-bs-toggle="modal" data-bs-target="#modal_create">
-      <img src="{{asset("images/add-icon.png")}}" class="add-button" alt="Adicionar servidor">
+    <div style="margin: auto"></div>
+    {{-- TODO: Falta adicionar um modal com os possiveis filtros  --}}
+    <form action="{{route("servidores.index")}}" method="GET" id="myForm">
+      <input type="text" onkeyup="" placeholder="Digite a busca" title="" id="valor" name="valor" style="background-color: #D9D9D9;
+              border-radius: 30px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+              background-position: 10px 2px;
+              background-repeat: no-repeat;
+              width: 35%;
+              font-size: 16px;
+              height: 45px;
+              border: 1px solid #ddd;
+              margin-bottom: 12px;
+              margin-right: 12px;">
+      <input type="submit" value="" style="background-image: url('/images/searchicon.png');
+              background-color: #D9D9D9;
+              border-radius: 30px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+              width: 50px;
+              font-size: 16px;
+              height: 45px;
+              border: 1px solid #ddd;
+              position: absolute;
+              margin: auto;" />
+    </form>
+  </div>
+  <div  style="display: contents; align-content: center; align-items: center">
+    <a style="background: #2D3875; border-radius: 25px; border: #2D3875; color: #f0f0f0; font-style: normal;
+      font-weight: 400; font-size: 15px; text-decoration: none; line-height: 28px; padding: 9px; align-content: center; align-items: center; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);" href="{{route('servidores.create')}}">
+      <img src="{{asset("images/plus.png")}}" alt="Cadastrar servidor" style="padding-bottom: 5px; width: 20px;"> Cadastrar servidor
     </a>
-  
-    @include("servidores.components.modal_create")
-  
-    @if (sizeof($servidores) == 0)
-      <div class="empty">
-        <p>
-          Não há servidores cadastrados
-        </p>
+  </div>
+
+ 
+
+  @if (sizeof($servidores) == 0)
+  <div class="empty">
+    <p>
+      Não há servidores cadastrados
+    </p>
+  </div>
+  @else
+  <br>
+  <div class="row p-5" style="display: flex; justify-content:center;">
+    <div class="col-10">
+      <div class="table-responsive" style="border: 1px solid #d3d3d3; border-radius: 15px; padding: 15px; background: #F2F2F2; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);">
+        <table class="table" style="background-color: #F2F2F2; ">
+          <thead>
+            <tr>
+              <th scope="col" style="border-right: 1px solid #d3d3d3;">Nome</th>
+              <th scope="col" style="border-right: 1px solid #d3d3d3;">Email</th>
+              <th scope="col" style="border-right: 1px solid #d3d3d3;">CPF</th>
+              <th scope="col" style="border-right: 1px solid #d3d3d3;">Tipo do servidor</th>
+              <th scope="col">Ações</th>
+            </tr>
+          </thead>
+          @foreach ($servidores as $servidor)
+          <tbody>
+            <tr>
+              <td class="item" style="border-right: 1px solid #d3d3d3;">{{$servidor->user->name}}</td>
+              <td class="item" style="border-right: 1px solid #d3d3d3;">{{$servidor->user->email}}</td>
+              <td class="item" style="border-right: 1px solid #d3d3d3;">{{$servidor->cpf}}</td>
+              <td class="item" style="border-right: 1px solid #d3d3d3;">{{$servidor->tipo_servidor}}</td>
+              <td>
+                <a type="button" data-bs-toggle="modal" data-bs-target="#modal_show_{{$servidor->id}}">
+                  <img src="{{asset("images/info.png")}}" alt="Info servidor">
+                </a>
+                <a type="button" data-bs-toggle="modal" data-bs-target="#modal_documents_{{$servidor->id}}">
+                  <img src="{{asset("images/document.png")}}" alt="Documento servidor">
+                  {{-- TODO: Fica pra fazer o modal depois  --}}
+                </a>
+                <a href="{{url("/servidores/$servidor->id/edit")}}">
+                  <img src="{{asset("images/edit-outline-blue.png")}}" alt="Editar servidor">
+                </a>
+                <a type="button" data-bs-toggle="modal" data-bs-target="#modal_delete_{{$servidor->id}}">
+                  <img src="{{asset("images/delete.png")}}" alt="Deletar servidor">
+                </a>
+              </td>
+            </tr>
+            @include("servidores.components.modal_show", ["servidor" => $servidor])
+            @include("servidores.components.modal_delete", ["servidor" => $servidor])
+            @endforeach
+          </tbody>
+        </table>
       </div>
-    @else
-      <div id="list">
-        @foreach ($servidores as $servidor)
-          <div class="row justify-content-md-center listing-card">
-            <div class="col-md-9 col-lg-9 informacoes">
-              <a type="button" class="ver" style="text-decoration: none; color: black;" onclick="exibirModalVisualizar({{$servidor->id}})">
-                <label class="labelIndex">{{$servidor->user->name}}</label>
-                <hr class="labelIndex">
-                <label class="labelIndex">Setor: {{$servidor->setor}}</label>
-              </a>
+        {{--  TODO: A tabela tem que limitar para aparecer x usuarios em cada página, e a paginação mudar o ultimo númerode acordo com o tanto de páginas que tem disponiveis para olhar  --}}   
+      <div style="margin: auto; padding: 10px; justify-content:center;">
+        <div class="pagination" style="display: flex; justify-content: center;">
+          <a href="#" style="border-radius: 15px; background: #131833; color: white;">Anterior</a>
+          <a href="#" style="border-radius: 15px; background: #34A853; color: white;">1</a>
+          <a href="#" style="border-radius: 15px; background: #34A853; color: white;">2</a>
+          <a href="#" style="border-radius: 15px; background: #34A853; color: white;">3</a>
+          <a href="#" style="border-radius: 15px; background: #131833; color: white;">Próximo</a>
+        </div>
+      </div>
+      @endif
+    </div>
+    <div class="col-2">
+      <div style="background-color: #F2F2F2; border-radius: 15px; width:150px;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); display: flex; justify-content: center; ">
+        <div class="text-start p-3 align-items-center">
+          <div class="text-center pb-3" >
+            <h4 style="font-size:15px;"><strong> Legenda dos icones: </strong></h4>
+          </div>
+          <div style="justify-content: center;">
+            <div style=" margin-bottom: 10px; ">
+              <a style="font-style:normal; font-weight: 400; font-size: 15px; line-height: 130%; "><img src="{{asset("images/searchicon.png")}}" alt="Procurar" style="width: 20px; margin-right:5px;">Pesquisar</a>
             </div>
-            <div class="col-md-2 col-lg-2 opcoes row">
-              <a type="button" class="col-md-auto edit" onclick="exibirModalEditar({{$servidor->id}})">
-                <img src="{{asset("images/editar.png")}}" class="option-button" alt="Editar servidor">
-              </a>
-              <a type="button" class="col-md-auto delete" onclick="exibirModalDeletar({{$servidor->id}})">
-                <img src="{{asset("images/excluir.png")}}" class="option-button" alt="Excluir servidor">
-              </a>
+            <div style=" margin-bottom: 10px">
+              <a style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%; "><img src="/images/info.png" alt="Informacoes" style="width: 20px; margin-right:5px;">Informações</a>
+            </div>
+            <div style=" margin-bottom: 10px">
+              <a style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%;"><img src="/images/document.png" alt="Documentos" style="width: 20px; margin-right:5px;">Documentos</a>
+            </div>
+            <div style=" margin-bottom: 10px">
+              <a style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%;"><img src="/images/edit-outline-blue.png" alt="Editar" style="width: 20px; margin-right:5px;">Editar</a>
+            </div>
+            <div style=" margin-bottom: 10px">
+              <a style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%; "><img src="{{asset("images/delete.png")}}" alt="Deletar aluno" style="width: 20px; margin-right:5px;">Deletar</a>
             </div>
           </div>
-          @include("servidores.components.modal_edit", ['servidor' => $servidor])
-          @include("servidores.components.modal_show")
-          @include("servidores.components.modal_delete")
-        @endforeach
+        </div>
       </div>
-    @endif
+    </div>
   </div>
-  
-  <script type="text/javascript">
+</div>
 
-    function exibirModalEditar(id){
-      $('#modal_edit_' + id).modal('show');
-    }
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
+</script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
 
-    function exibirModalDeletar(id){
-      $('#modal_delete_' + id).modal('show');
-    }
+<script>
+  $document.ready(function() {
+    $("#example").DataTable();
+  })
+</script>
 
-    function exibirModalVisualizar(id){
-      $('#modal_show_' + id).modal('show');
-    }
-  </script>
+<script type="text/javascript">
+  function exibirModalDeletar(id) {
+    $('#modal_delete_' + id).modal('show');
+  }
 
- <!-- Exibindo erros de validacao ao criar -->
- @if(count($errors->create) > 0)
-  <script type="text/javascript">
-    $(function () {
-      // Bloqueando o usuario na tela de modal apos falha na validacao.
-      // Forcando ele a clicar no botao de fechar, para limpar os erros
-      $("#modal_create").modal({backdrop:"static", keyboard:false});
-      $("#modal_create").modal('show');
-    });
-  </script>
-  @endif
+  function exibirModalVisualizar(id) {
+    $('#modal_show_' + id).modal('show');
+  }
+</script>
 
-  <!-- Exibindo erros de validacao ao editar -->
-  @if(count($errors->update) > 0)
-  <script type="text/javascript">
-    $(function () {
-      // Bloqueando o usuario na tela de modal apos falha na validacao.
-      // Forcando ele a clicar no botao de fechar, para limpar os erros
-      $("#modal_edit_{{old('id')}}").modal({backdrop:"static", keyboard:false});
-      $("#modal_edit_{{old('id')}}").modal('show');
-    });
-  </script>
-  @endif
+<style>
+  pagination {
+    display: flex;
+    justify-content: center;
+  }
 
-  @elsecan('aluno')
-    <h3 style="margin-top: 1rem">Você não possui permissão!</h3>
-    <a class="btn btn-primary submit" style="margin-top: 1rem" href="{{route('home')}}">Voltar</a>
-  @else
-    <h3 style="margin-top: 1rem">Você não possui permissão!</h3>
-    <a class="btn btn-primary submit" style="margin-top: 1rem" href="{{url("/login")}}">Voltar</a>
-  @endcan
- @endsection
- 
+  .pagination a {
+    color: black;
+    float: left;
+    padding: 8px 16px;
+    text-decoration: none;
+    transition: background-color .3s;
+    border: 1px solid #ddd;
+    margin: 10px 4px;
+  }
+
+  .pagination a.active {
+    background-color: #3B864F;
+    color: white;
+    border: 1px solid #3B864F;
+  }
+
+  .pagination a:hover:not(.active) {
+    background-color: #34A853;
+  }
+  ::-webkit-input-placeholder {
+  text-align: center;
+  }
+
+</style>
+
+@elsecan('servidor')
+<h3 style="margin-top: 1rem">Você não possui permissão!</h3>
+<a class="btn btn-primary submit" style="margin-top: 1rem" href="{{route('home')}}">Voltar</a>
+@else
+<h3 style="margin-top: 1rem">Você não possui permissão!</h3>
+<a class="btn btn-primary submit" style="margin-top: 1rem" href="{{url("/login")}}">Voltar</a>
+@endcan
+@endsection
