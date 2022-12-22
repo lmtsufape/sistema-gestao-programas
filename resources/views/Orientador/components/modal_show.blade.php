6b3da8063f95cb3f5f7124dedb7c94ee781f929e
Camila Claudino <camila.claudino@upe.br>
2022-12-22 17:53:14 -0300

    Criação da tela de listar orientador + modais

--- resources/views/Orientador/components/modal_show.blade.php
+++ resources/views/Orientador/components/modal_show.blade.php
@@ -13,17 +13,17 @@
             </div> 
            
             <div class="col-sm- 12 col-md-6 mb-3">
-              <label class="form-label">CPF</label>
+              <label class="form-label"><strong>CPF</strong></label>
               <div class="modal-ver" style="background: #F5F5F5; padding:3px; height: 30px; box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.25); border-radius: 45px;">{{$orientador->cpf}}</div>
             </div> 
 
             <div class="col-sm- 12 col-md-6 mb-3">
-              <label class="form-label">E-mail</label>
+              <label class="form-label"><strong>E-mail</strong></label>
               <div class="modal-ver" style="background: #F5F5F5; padding:3px; height: 30px; box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.25); border-radius: 45px;">{{$orientador->user->email}}</div>
             </div> 
 
             <div class="col-sm- 12 col-md-6 mb-3">
-              <label class="form-label">Matrícula</label>
+              <label class="form-label"><strong>Matrícula</strong></label>
               <div class="modal-ver" style="background: #F5F5F5; padding:3px; height: 30px; box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.25); border-radius: 45px;">{{$orientador->matricula}}</div>
             </div>
           </div>
