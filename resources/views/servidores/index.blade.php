c92ff2ef8d152453b727ce97842ef8b301e090e6
Camila Claudino <camila.claudino@upe.br>
2022-12-22 17:00:27 -0300

    Criação da tela de listar servidor + modais
    Foram criados a tela de listar servidor junto com os modais de visualizar e deletar.

--- resources/views/servidores/index.blade.php
+++ resources/views/servidores/index.blade.php
@@ -34,14 +34,15 @@
               margin: auto;" />
     </form>
   </div>
-  <div  style="display: contents; align-content: center; align-items: center">
+  <div style="display: contents; align-content: center; align-items: center;">
     <a style="background: #2D3875; border-radius: 25px; border: #2D3875; color: #f0f0f0; font-style: normal;
-      font-weight: 400; font-size: 15px; text-decoration: none; line-height: 28px; padding: 9px; align-content: center; align-items: center; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);" href="{{route('servidores.create')}}">
-      <img src="{{asset("images/plus.png")}}" alt="Cadastrar servidor" style="padding-bottom: 5px; width: 20px;"> Cadastrar servidor
+    font-weight: 400; font-size: 24px; line-height: 28px; padding-top: 6px; padding-bottom: 6px; align-content: center;
+    align-items: center; padding-right: 15px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); text-decoration: none;
+    padding-left: 10px;" href="{{route('servidores.create')}}">
+    <img src="{{asset("images/plus.png")}}" alt="Cadastrar servidor" style="padding-bottom: 5px;"> Cadastrar servidor
     </a>
+    <br>
   </div>
-
- 
 
   @if (sizeof($servidores) == 0)
   <div class="empty">
@@ -51,99 +52,89 @@
   </div>
   @else
   <br>
-  <div class="row p-5" style="display: flex; justify-content:center;">
-    <div class="col-10">
-      <div class="table-responsive" style="border: 1px solid #d3d3d3; border-radius: 15px; padding: 15px; background: #F2F2F2; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);">
-        <table class="table" style="background-color: #F2F2F2; ">
-          <thead>
-            <tr>
-              <th scope="col" style="border-right: 1px solid #d3d3d3;">Nome</th>
-              <th scope="col" style="border-right: 1px solid #d3d3d3;">Email</th>
-              <th scope="col" style="border-right: 1px solid #d3d3d3;">CPF</th>
-              <th scope="col" style="border-right: 1px solid #d3d3d3;">Tipo do servidor</th>
-              <th scope="col">Ações</th>
-            </tr>
-          </thead>
-          @foreach ($servidores as $servidor)
-          <tbody>
-            <tr>
-              <td class="item" style="border-right: 1px solid #d3d3d3;">{{$servidor->user->name}}</td>
-              <td class="item" style="border-right: 1px solid #d3d3d3;">{{$servidor->user->email}}</td>
-              <td class="item" style="border-right: 1px solid #d3d3d3;">{{$servidor->cpf}}</td>
-              <td class="item" style="border-right: 1px solid #d3d3d3;">{{$servidor->tipo_servidor}}</td>
-              <td>
-                <a type="button" data-bs-toggle="modal" data-bs-target="#modal_show_{{$servidor->id}}">
-                  <img src="{{asset("images/info.png")}}" alt="Info servidor">
-                </a>
-                <a type="button" data-bs-toggle="modal" data-bs-target="#modal_documents_{{$servidor->id}}">
-                  <img src="{{asset("images/document.png")}}" alt="Documento servidor">
-                  {{-- TODO: Fica pra fazer o modal depois  --}}
-                </a>
-                <a href="{{url("/servidores/$servidor->id/edit")}}">
-                  <img src="{{asset("images/edit-outline-blue.png")}}" alt="Editar servidor">
-                </a>
-                <a type="button" data-bs-toggle="modal" data-bs-target="#modal_delete_{{$servidor->id}}">
-                  <img src="{{asset("images/delete.png")}}" alt="Deletar servidor">
-                </a>
-              </td>
-            </tr>
-            @include("servidores.components.modal_show", ["servidor" => $servidor])
-            @include("servidores.components.modal_delete", ["servidor" => $servidor])
-            @endforeach
-          </tbody>
-        </table>
+  <div style="display: flex; gap: 30px;">
+    <table class="table" style="border-radius: 15px; background-color: #F2F2F2; min-width: 600px; box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25)
+    ;margin-bottom: 5px; min-height: 350px">
+      <thead>
+        <tr>
+          <th scope="col" style="border-right: 1px solid #d3d3d3;">Nome</th>
+          <th scope="col" style="border-right: 1px solid #d3d3d3;">Email</th>
+          <th scope="col" style="border-right: 1px solid #d3d3d3;">CPF</th>
+          <th scope="col" style="border-right: 1px solid #d3d3d3;">Tipo do servidor</th>
+          <th scope="col">Ações</th>
+        </tr>
+      </thead>
+      @foreach ($servidores as $servidor)
+      <tbody>
+        <tr>
+          <td style="border-right: 1px solid #d3d3d3;">{{$servidor->user->name}}</td>
+            <td style="border-right: 1px solid #d3d3d3;">{{$servidor->user->email}}</td>
+            <td style="border-right: 1px solid #d3d3d3;">{{$servidor->cpf}}</td>
+            <td style="border-right: 1px solid #d3d3d3;">{{$servidor->tipo_servidor}}</td>
+            <td>
+              <a type="button" data-bs-toggle="modal" data-bs-target="#modal_show_{{$servidor->id}}">
+                <img src="{{asset("images/info.png")}}" alt="Info servidor">
+              </a>
+              <a type="button" data-bs-toggle="modal" data-bs-target="#modal_documents_{{$servidor->id}}">
+                <img src="{{asset("images/document.png")}}" alt="Documento servidor">
+                {{-- TODO: Fica pra fazer o modal depois  --}}
+              </a>
+              <a href="{{url("/servidores/$servidor->id/edit")}}">
+                <img src="{{asset("images/edit-outline-blue.png")}}" alt="Editar servidor">
+              </a>
+              <a type="button" data-bs-toggle="modal" data-bs-target="#modal_delete_{{$servidor->id}}">
+                <img src="{{asset("images/delete.png")}}" alt="Deletar servidor">
+              </a>
+            </td>
+        </tr>
+        @include("servidores.components.modal_show", ["servidor" => $servidor])
+        @include("servidores.components.modal_delete", ["servidor" => $servidor])
+        @endforeach
+      </tbody>
+    </table>
+    <div style="background-color: #F2F2F2; border-radius: 15px; justify-content: center; align-items: center; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); width: 150px; height: 40%;">
+      <div style="align-self: center; margin-right: auto">
+        <br>
+        <h4 style="font-size: 15px">Legenda dos ícones:</h4>
       </div>
-        {{--  TODO: A tabela tem que limitar para aparecer x usuarios em cada página, e a paginação mudar o ultimo númerode acordo com o tanto de páginas que tem disponiveis para olhar  --}}   
-      <div style="margin: auto; padding: 10px; justify-content:center;">
-        <div class="pagination" style="display: flex; justify-content: center;">
-          <a href="#" style="border-radius: 15px; background: #131833; color: white;">Anterior</a>
-          <a href="#" style="border-radius: 15px; background: #34A853; color: white;">1</a>
-          <a href="#" style="border-radius: 15px; background: #34A853; color: white;">2</a>
-          <a href="#" style="border-radius: 15px; background: #34A853; color: white;">3</a>
-          <a href="#" style="border-radius: 15px; background: #131833; color: white;">Próximo</a>
+      <div style="align-self: center; margin-right: auto">
+        <div style="display: flex; margin: 10px">
+          <a><img src="{{asset("images/searchicon.png")}}" alt="Procurar" style="width: 20px; height: 20px;"></a>
+          <p style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%; margin:5px">Pesquisar</p>
+        </div>
+        <div style="display: flex; margin: 10px">
+          <a><img src="/images/info.png" alt="Informacoes" style="width: 20px; height: 20px;"></a>
+          <p style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%; margin:5px">Informações</p>
+        </div>
+        <div style="display: flex; margin: 10px">
+          <a><img src="/images/document.png" alt="Documentos" style="width: 20px; height: 20px;"></a>
+          <p style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%; margin:5px">Documentos</p>
         </div>
       </div>
-      @endif
+      <div style="align-self: center; margin-right: auto">
+        <div style="display: flex; margin: 10px">
+          <a><img src="/images/edit-outline-blue.png" alt="Editar" style="width: 20px; height: 20px;"></a>
+          <p style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%; margin:5px">Editar</p>
+        </div>
+        <div style="display: flex; margin: 10px">
+          <a><img src="{{asset("images/delete.png")}}" alt="Deletar aluno" style="width: 20px; height: 20px;"></a>
+          <p style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%; margin:5px">Deletar</p>
+        </div>
+      </div>
     </div>
-    <div class="col-2">
-      <div style="background-color: #F2F2F2; border-radius: 15px; width:150px;
-        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); display: flex; justify-content: center; ">
-        <div class="text-start p-3 align-items-center">
-          <div class="text-center pb-3" >
-            <h4 style="font-size:15px;"><strong> Legenda dos icones: </strong></h4>
-          </div>
-          <div style="justify-content: center;">
-            <div style=" margin-bottom: 10px; ">
-              <a style="font-style:normal; font-weight: 400; font-size: 15px; line-height: 130%; "><img src="{{asset("images/searchicon.png")}}" alt="Procurar" style="width: 20px; margin-right:5px;">Pesquisar</a>
-            </div>
-            <div style=" margin-bottom: 10px">
-              <a style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%; "><img src="/images/info.png" alt="Informacoes" style="width: 20px; margin-right:5px;">Informações</a>
-            </div>
-            <div style=" margin-bottom: 10px">
-              <a style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%;"><img src="/images/document.png" alt="Documentos" style="width: 20px; margin-right:5px;">Documentos</a>
-            </div>
-            <div style=" margin-bottom: 10px">
-              <a style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%;"><img src="/images/edit-outline-blue.png" alt="Editar" style="width: 20px; margin-right:5px;">Editar</a>
-            </div>
-            <div style=" margin-bottom: 10px">
-              <a style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%; "><img src="{{asset("images/delete.png")}}" alt="Deletar aluno" style="width: 20px; margin-right:5px;">Deletar</a>
-            </div>
-          </div>
-        </div>
-      </div>
+    @endif
+  </div>
+  {{--  TODO: A tabela tem que limitar para aparecer x usuarios em cada página, e a paginação mudar o ultimo númerode acordo com o tanto de páginas que tem disponiveis para olhar  --}}   
+  <div style="margin: auto; padding: 10px; justify-content:center;">
+    <div class="pagination" style="display: flex; justify-content: center;">
+      <a href="#" style="border-radius: 15px; background: #131833; color: white;">Anterior</a>
+      <a href="#" style="border-radius: 15px; background: #34A853; color: white;">1</a>
+      <a href="#" style="border-radius: 15px; background: #34A853; color: white;">2</a>
+      <a href="#" style="border-radius: 15px; background: #34A853; color: white;">3</a>
+      <a href="#" style="border-radius: 15px; background: #131833; color: white;">Próximo</a>
     </div>
   </div>
 </div>
-
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
-</script>
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
-
-<script>
-  $document.ready(function() {
-    $("#example").DataTable();
-  })
-</script>
 
 <script type="text/javascript">
   function exibirModalDeletar(id) {
