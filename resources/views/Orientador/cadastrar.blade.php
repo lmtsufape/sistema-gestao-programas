82c926e0189bd0cb9631eb73e5036df62c770639
Anna Victoria Feliciana <anna.feliciana@upe.br>
2022-12-30 15:03:34 -0300

    placeholder cadastro orientador

--- resources/views/Orientador/cadastrar.blade.php
+++ resources/views/Orientador/cadastrar.blade.php
@@ -30,30 +30,30 @@
         <form action="{{route("orientadors.store")}}" method="POST">
             @csrf
             <label for="nome" style="display:flex; font-weight: 600; font-size: 20px; line-height: 28px; color: #131833;">Nome: </label>
-            <input type="text" name="nome" id="nome"
+            <input type="text" name="nome" id="nome" placeholder="Digite o nome"
             style="background: #F5F5F5; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px;
             box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);"><br><br>
 
             <label for="email" style="display:flex; font-weight: 600; font-size: 20px; line-height: 28px; color: #131833;">E-mail: </label>
-            <input type="text" name="email" id="email"
+            <input type="text" name="email" id="email" placeholder="Digite o e-mail"
             style="background: #F5F5F5; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px;
             box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);"><br><br>
 
             <label for="senha" style="display:flex; font-weight: 600; font-size: 20px; line-height: 28px; color: #131833;">Senha: </label>
-            <input type="password" name="senha" id="senha"
+            <input type="password" name="senha" id="senha" placeholder="Digite a senha"
             style="background: #F5F5F5; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px;
             box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);"><br><br>
 
             <label for="cpf" style="display:flex; font-weight: 600; font-size: 20px; line-height: 28px; color: #131833;">CPF: </label>
-            <input type="text" name="cpf" id="cpf"
+            <input type="text" name="cpf" id="cpf" placeholder="Digite o CPF"
             style="background: #F5F5F5; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px;
             box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);"><br><br>
 
             <label for="matricula" style="display:flex; font-weight: 600; font-size: 20px; line-height: 28px; color: #131833;">Matrícula: </label>
-            <input type="text" name="matricula" id="matricula"
+            <input type="text" name="matricula" id="matricula" placeholder="Digite a matrícula"
             style="background: #F5F5F5; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px;
              box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);"><br><br>
-            
+
             <div style="display: flex; align-content: center; align-items: center; justify-content: center; gap:5%">
                 <input type="button" value="Voltar" href="{{route('home')}}" onclick="window.location.href='{{route('home')}}'"
                 style="background: #2D3875; box-shadow: 4px 5px 7px rgba(0, 0, 0, 0.25); display: inline-block;
@@ -65,7 +65,7 @@
                 border-radius: 13px; color: #FFFFFF; border: #34A853; font-style: normal; font-weight: 400; font-size: 24px;
                 line-height: 29px; text-align: center; padding: 5px 15px;">
             </div>
-        </form>        
+        </form>
     </div>
 </div>
 
