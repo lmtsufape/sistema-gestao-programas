b2296f2e3c3ee020f299338f64147a0ca9a59179
victorMendes18 <victorfran18@gmail.com>
2023-01-09 22:13:07 -0300

    listar edital

--- app/Http/Controllers/EditalController.php
+++ app/Http/Controllers/EditalController.php
@@ -9,6 +9,8 @@ use App\Models\Curso;
 use App\Models\Programa;
 use App\Models\Orientador;
 use App\Models\Edital_orientador;
+use App\Models\Edital_aluno;
+use App\Models\Frequencia_mensal;
 use App\Http\Requests\EditalStoreFormRequest;
 use App\Http\Requests\EditalUpdateFormRequest;
 
@@ -21,7 +23,8 @@ class EditalController extends Controller
      */
     public function index()
     {
-        //
+        $editals = Edital::all();
+        return view("Edital.index", compact('editals'));
     }
 
     /**
@@ -65,7 +68,7 @@ class EditalController extends Controller
 
             DB::commit();
 
-            return redirect('/editals/create')->with('sucesso', 'Edital cadastrado com sucesso.');
+            return redirect('/editals')->with('sucesso', 'Edital cadastrado com sucesso.');
 
         } catch(exception $e){
             DB::rollback();
@@ -136,7 +139,7 @@ class EditalController extends Controller
 
             DB::commit();
 
-            return redirect("/editals/$edital->id/edit")->with('sucesso', 'Edital editado com sucesso.');
+            return redirect("/editals")->with('sucesso', 'Edital editado com sucesso.');
 
         } catch(exception $e){
             DB::rollback();
@@ -152,6 +155,27 @@ class EditalController extends Controller
      */
     public function destroy($id)
     {
-        //
+        DB::beginTransaction();
+        try{
+            $edital = Edital::find($id);
+
+            Edital_orientador::where("id_edital", $id)->delete();
+
+            foreach($edital->edital_alunos as $edital_aluno){
+                Frequencia_mensal::where("id_edital_aluno", $edital_aluno->id)->delete();
+            }
+
+            Edital_aluno::where("id_edital", $id)->delete();
+
+            Edital::where("id", $id)->delete();
+
+            DB::commit();
+
+            return redirect("/editals")->with('sucesso', 'Edital deletado com sucesso.');
+
+        } catch(exception $e){
+            DB::rollback();
+            return redirect()->back()->withErrors( "Falha ao editar Edital. tente novamente mais tarde." );
+        }
     }
 }
