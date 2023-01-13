0495cb4e100f816ec677b2adde8915c9312f0bad
Anna Victoria Feliciana <anna.feliciana@upe.br>
2023-01-13 00:32:37 -0300

    mudanças no front do edital

--- resources/views/Edital/index.blade.php
+++ resources/views/Edital/index.blade.php
@@ -2,17 +2,45 @@
 
 @section("body")
 
+<style>
+    pagination {
+      display: inline-block;
+
+    }
+
+    .pagination a {
+      color: black;
+      float: left;
+      padding: 8px 16px;
+      text-decoration: none;
+      transition: background-color .3s;
+      border: 1px solid #ddd;
+      margin: 10px 4px;
+    }
+
+    .pagination a.active {
+      background-color: #3B864F;
+      color: white;
+      border: 1px solid #3B864F;
+    }
+
+    .pagination a:hover:not(.active) {
+      background-color: #34A853;
+    }
+
+  </style>
+
     <div class="container">
         @if (session('sucesso'))
             <div class="alert alert-success">
                 {{session('sucesso')}}
             </div>
         @endif
-    </div><br>
+    <br>
 
     <div style="margin-bottom: 10px;  gap: 20px; margin-top: 20px">
         <h1><strong>Editais</strong></h1>
-
+        <div style="margin: auto"></div>
         <form action="{{route("editals.index")}}" method="GET">
             <input type="text" onkeyup="" placeholder="Digite a busca" title="" id="valor" name="valor"
             style="background-color: #D9D9D9;
@@ -130,6 +158,7 @@
           </div>
         </div>
     </div>
+</div>
 
     <script type="text/javascript">
 
