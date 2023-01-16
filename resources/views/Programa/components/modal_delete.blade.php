bb27559dabbcd743666a76f9b88e0b990e65f684
Camila Claudino <camila.claudino@upe.br>
2023-01-16 17:15:13 -0300

    Correção do modal de deletar programa

--- resources/views/Programa/components/modal_delete.blade.php
+++ resources/views/Programa/components/modal_delete.blade.php
@@ -1,21 +1,21 @@
 <div class="modal fade" id="modal_delete_{{$programa->id}}" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
+    <div class="modal-content" style="border-radius: 15px; background-color: #F2F2F2; font-family: 'Roboto', sans-serif;">
       <div class="modal-header">
-        <h5 style="color: #131833; font-style: normal; font-weight: 600; font-size: 30px; line-height: 47px;">Confirmação</h5>
+        <h5 class="modal-title title fw-bold " style="color: #131833; font-size: 25px; line-height: 47px;">Deletar Programa</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <h5 style="color: #131833; font-style: normal; font-weight: 600; font-size: 20px; line-height: 47px;">
-          Deseja realmente deletar o programa {{$programa->nome}}?</h5>
+        <p style="color: #131833; font-style: normal; font-weight: 400; font-size: 20px; line-height: 47px;">
+          Deseja realmente deletar o programa {{$programa->nome}}?</p>
       </div>
-      <div class="modal-footer">
+      <div class="modal-footer d-flex justify-content-between">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
         <form action="{{url("/programas/>1")}}" method="post">
           @method("DELETE")
           @csrf
           <input type="hidden" name="id" value="{{$programa->id}}">
-          <button type="submit" class="btn btn-danger">Confirmar exclusão</button>
+          <button type="submit" class="btn btn-danger">Deletar</button>
         </form>
       </div>
     </div>
