252b8e25b32eeb9717f0f2ead3ca367bd1853900
Camila Claudino <camila.claudino@upe.br>
2023-01-16 17:17:06 -0300

    Correção do modal de visualizar programa

--- resources/views/Programa/components/modal_show.blade.php
+++ resources/views/Programa/components/modal_show.blade.php
@@ -1,19 +1,16 @@
 <div class="modal fade" id="modal_show_{{$programa->id}}" tabindex="-1" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content modal-create">
+    <div class="modal-content modal-create p-3" style="border-radius: 15px; background-color: #EEEEEE; font-family: 'Roboto', sans-serif;">
       <div class="modal-header">
-
-        <h5 style="color: #131833; font-style: normal; font-weight: 600;
-        font-size: 30px; line-height: 47px;">{{$programa->nome}}</h5>
-
+        <h5 style="color: #131833; font-style: normal; font-weight: 600; font-size: 30px; line-height: 47px;" class="modal-title title fw-bold">Informações do Programa</h5>
         <button class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-        <div class="modal-body">
-            <div class="mb-3" style="">
-                <label for="nome_edit" style="display:flex; font-weight: 600; font-size: 20px; line-height: 28px; color: #131833;">
+      <div class="modal-body">
+          <div class="row mb-3">
+                <label for="nome_edit" style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833;" class="form-label mt-3">
                 Nome:</label>
 
-                <div style="background: #F5F5F5; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px">
+                <div style="background: #F9F9F9; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px">
                 {{$programa->nome}}</div>
                 @if(!empty($errors->update->first('name')))
                       <span class="invalid-feedback d-block">
@@ -21,8 +18,6 @@
                       </span>
                 @endif
               </div>
-          <p></p>
-          {{--  <a class="btn btn-primary submit-button" data-bs-dismiss="modal" style="width: 200px" role="button">Voltar</a>  --}}
         </div>
     </div>
   </div>
