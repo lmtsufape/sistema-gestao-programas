ecf3b3c6f6083bc9c488f15353a9affe90a41804
Anna Victoria Feliciana <anna.feliciana@upe.br>
2023-01-17 20:50:19 -0300

    Update input cadastrar aluno

--- resources/views/Alunos/cadastro-aluno.blade.php
+++ resources/views/Alunos/cadastro-aluno.blade.php
@@ -1,6 +1,60 @@
 @extends("templates.app")
 
 @section("body")
+<style>
+    select[multiple] {
+        overflow: hidden;
+        background: #f5f5f5;
+        width:100%;
+        height:auto;
+        padding: 0px 5px;
+        margin:0;
+        border-width: 2px;
+        border-radius: 5px;
+        -moz-appearance: menulist;
+        -webkit-appearance: menulist;
+        appearance: menulist;
+      }
+      /* select single */
+      .required .chosen-single {
+        background: #F5F5F5;
+        border-radius: 5px;
+        border: 1px #D3D3D3;
+        padding: 5px;
+        box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);
+    }
+    /* select multiple */
+    .required .chosen-choices {
+        background: #F5F5F5;
+        border-radius: 5px;
+        border: 1px #D3D3D3;
+        padding: 0px 5px;
+        box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);
+    }
+    .titulo {
+        font-weight: 600;
+        font-size: 20px;
+        line-height: 28px;
+        display: flex;
+        color: #131833;
+    }
+    .boxinfo{
+        background: #F5F5F5;
+        border-radius: 6px;
+        bborder: 1px #D3D3D3;
+        width: 100%;
+        padding: 5px;
+        box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);
+
+    }
+    .boxchild{
+        background: #FFFFFF;
+        box-shadow: 0px 6px 20px rgba(0, 0, 0, 0.25);
+        border-radius: 20px;
+        padding: 34px;
+        width: 65%
+    }
+</style>
 
 <div class="container" style="display: flex; justify-content: center; align-items: center; margin-top: 1em; margin-bottom:10px">
     @if ($errors->any())
@@ -19,7 +73,7 @@
     </div>
     @endif
     <br>
-    <div style="background: #FFFFFF; box-shadow: 0px 6px 20px rgba(0, 0, 0, 0.25); border-radius: 20px; padding: 34px; width: 65%">
+    <div class="boxchild">
         <div class="row">
             <h1 style="font-weight: 600; font-size: 30px; line-height: 47px; display: flex; align-items: center; color: #131833;">
                 Cadastrar Aluno</h1>
@@ -29,43 +83,37 @@
 
         <form action="{{route("alunos.store")}}" method="POST">
             @csrf
-            <label for="inputName" style="display:flex; font-weight: 600; font-size: 20px; line-height: 28px; color: #131833;" class="form-label ">Nome:</label>
-            <input type="text" style="background: #F5F5F5; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px;
-            box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);" class="form-control" id="inputName" name="nome" required placeholder="Digite o nome">
-            <div class="invalid-feedback"> Por favor preencha esse campo</div><br>
+            <label for="inputName" class="titulo">Nome:</label>
+            <input class="boxinfo" type="text" id="inputName" name="nome" required placeholder="Digite o nome">
+            <div class="invalid-feedback"> Por favor preencha esse campo</div><br><br>
 
 
-            <label for="inputCpf" style="display:flex; font-weight: 600; font-size: 20px; line-height: 28px; color: #131833;" class="form-label ">CPF:</label>
-            <input type="text" style="background: #F5F5F5; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px;
-            box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);" class="form-control" id="inputCpf" name="cpf" required placeholder="Digite o CPF">
-            <div class="invalid-feedback"> Por favor preencha esse campo</div><br>
+            <label for="inputCpf" class="titulo">CPF:</label>
+            <input class="boxinfo" type="text"  id="inputCpf" name="cpf" required placeholder="Digite o CPF">
+            <div class="invalid-feedback"> Por favor preencha esse campo</div><br><br>
 
 
-            <label for="inputSemestre" style="display:flex; font-weight: 600; font-size: 20px; line-height: 28px; color: #131833;" class="form-label ">Semestre de Entrada:</label>
-            <input type="text" style="background: #F5F5F5; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px;
-            box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);" class="form-control" id="inputSemestre" name="semestre_entrada" required placeholder="Digite o semestre">
-            <div class="invalid-feedback"> Por favor preencha esse campo</div> <br>
+            <label for="inputSemestre" class="titulo">Semestre de Entrada:</label>
+            <input class="boxinfo" type="text"  id="inputSemestre" name="semestre_entrada" required placeholder="Digite o semestre">
+            <div class="invalid-feedback"> Por favor preencha esse campo</div><br><br>
 
 
-            <label for="inputCurso" style="display:flex; font-weight: 600; font-size: 20px; line-height: 28px; color: #131833;" class="form-label ">Curso:</label>
-            <select style="background: #F5F5F5; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px;
-            box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);" class="form-control" id="inputCurso" name="curso">
+            <label for="inputCurso" class="titulo">Curso:</label>
+            <select aria-label="Default select example" class="boxinfo">      id="inputCurso" name="curso">
                 <option value="">Selecione o curso</option>
                 @foreach ($cursos as $curso)
                 <option value="{{$curso->id}}">{{$curso->nome}}</option>
                 @endforeach
             </select>
-            <div class="invalid-feedback"> Por favor preencha esse campo</div><br>
-
-            <label for="inputEmail4" style="display:flex; font-weight: 600; font-size: 20px; line-height: 28px; color: #131833;" class="form-label ">Email:</label>
-            <input type="email" style="background: #F5F5F5; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px;
-            box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);" id="inputEmail4" name="email" required placeholder="Digite o email">
             <div class="invalid-feedback"> Por favor preencha esse campo</div><br><br>
 
-            <label for="inputPassword4" style="display:flex; font-weight: 600; font-size: 20px; line-height: 28px; color: #131833;" class="form-label ">Senha:</label>
-            <input type="password" style="background: #F5F5F5; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px;
-                            box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);" class="form-control" id="inputPassword4" name="senha" required placeholder="Digite a senha">
-            <div class="invalid-feedback"> Por favor preencha esse campo</div><br>
+            <label for="inputEmail4" class="titulo">Email:</label>
+            <input class="boxinfo" type="email" id="inputEmail4" name="email" required placeholder="Digite o email">
+            <div class="invalid-feedback"> Por favor preencha esse campo</div><br><br>
+
+            <label for="inputPassword4" class="titulo">Senha:</label>
+            <input type="password"  class="boxinfo" id="inputPassword4" name="senha" required placeholder="Digite a senha">
+            <div class="invalid-feedback"> Por favor preencha esse campo</div><br><br>
 
             <div style="display: flex; align-content: center; align-items: center; justify-content: center; gap:5%">
                 <input type="button" value="Voltar" href="{{url("/alunos/")}}" onclick="window.location.href='{{url("/alunos/")}}'" style="background: #2D3875;
