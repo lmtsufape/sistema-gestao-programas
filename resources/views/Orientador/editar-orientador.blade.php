651f160258b22bb7b90d5cbd7c4dcb33cd95e75c
Anna Victoria Feliciana <anna.feliciana@upe.br>
2023-01-17 20:40:37 -0300

    Update input editar orientador

--- resources/views/Orientador/editar-orientador.blade.php
+++ resources/views/Orientador/editar-orientador.blade.php
@@ -2,6 +2,61 @@
 
 @section("body")
 
+<style>
+    select[multiple] {
+        overflow: hidden;
+        background: #f5f5f5;
+        width:100%;
+        height:auto;
+        padding: 0px 5px;
+        margin:0;
+        border-width: 2px;
+        border-radius: 5px;
+        -moz-appearance: menulist;
+        -webkit-appearance: menulist;
+        appearance: menulist;
+      }
+      /* select single */
+      .required .chosen-single {
+        background: #F5F5F5;
+        border-radius: 5px;
+        border: 1px #D3D3D3;
+        padding: 5px;
+        box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);
+    }
+    /* select multiple */
+    .required .chosen-choices {
+        background: #F5F5F5;
+        border-radius: 5px;
+        border: 1px #D3D3D3;
+        padding: 0px 5px;
+        box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);
+    }
+    .titulo {
+        font-weight: 600;
+        font-size: 20px;
+        line-height: 28px;
+        display: flex;
+        color: #131833;
+    }
+    .boxinfo{
+        background: #F5F5F5;
+        border-radius: 6px;
+        bborder: 1px #D3D3D3;
+        width: 100%;
+        padding: 5px;
+        box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);
+
+    }
+    .boxchild{
+        background: #FFFFFF;
+        box-shadow: 0px 6px 20px rgba(0, 0, 0, 0.25);
+        border-radius: 20px;
+        padding: 34px;
+        width: 65%
+    }
+</style>
+
 <div class="container" style="display: flex; justify-content: center; align-items: center; margin-top: 1em; margin-bottom:10px">
 
     @if (session('sucesso'))
@@ -10,35 +65,32 @@
         </div>
     @endif
     <br>
-    
-    <div style="background: #FFFFFF; box-shadow: 0px 6px 20px rgba(0, 0, 0, 0.25); border-radius: 20px; padding: 34px; width: 65%";>
-        <h1 style="font-weight: 600; font-size: 30px; line-height: 47px; display: flex; align-items: center; color: #131833;">
-            Editar orientador</h1>
-            <hr>
+
+    <div class="boxchild">
+        <div class="row">
+            <h1 style="font-weight: 600; font-size: 30px; line-height: 47px; display: flex; align-items: center; color: #131833;">
+                Editar Orientador</h1>
+        </div>
+
+        <hr>
         <form action="{{url("/orientadors/$orientador->id")}}" method="POST">
             @csrf
             @method("PUT")
-            <label for="nome" style="display:flex; font-weight: 600; font-size: 20px; line-height: 28px; color: #131833;">Nome:</label>
-            <input type="text" id="nome" name="nome" placeholder="Digite o nome" value="{{$orientador->user->name}}" 
-            style="background: #F5F5F5; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px;
-            box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);"><br/><br>
+            <label for="nome" class="titulo">Nome:</label>
+            <input class="boxinfo" type="text" id="nome" name="nome" placeholder="Digite o nome" value="{{$orientador->user->name}}"
+            ><br/><br>
 
-            <label for="email" for="nome" style="display:flex; font-weight: 600; font-size: 20px; line-height: 28px; color: #131833;">E-mail:</label>
-            <input type="text" id="email" name="email" placeholder="Digite o e-mail" value="{{$orientador->user->email}}" style="background: #F5F5F5; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px;
-            box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);"><br/><br>
+            <label for="email" for="nome" class="titulo">E-mail:</label>
+            <input class="boxinfo" type="text" id="email" name="email" placeholder="Digite o e-mail" value="{{$orientador->user->email}}"><br/><br>
 
-            <label for="senha" for="nome" style="display:flex; font-weight: 600; font-size: 20px; line-height: 28px; color: #131833;">Senha:</label>
-            <input type="password" id="senha" name="senha" placeholder="Digite a senha" style="background: #F5F5F5; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px;
-            box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);"><br/><br>
+            <label for="senha" for="nome" class="titulo">Senha:</label>
+            <input class="boxinfo" type="password" id="senha" name="senha" placeholder="Digite a senha"><br/><br>
 
-            <label for="cpf" for="nome" style="display:flex; font-weight: 600; font-size: 20px; line-height: 28px; color: #131833;">CPF:</label>
-            <input name="cpf" id="cpf" type="text" placeholder="Digite o CPF" value="{{$orientador->cpf}}" style="background: #F5F5F5; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px;
-            box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);"><br/><br>
+            <label for="cpf" for="nome" class="titulo">CPF:</label>
+            <input class="boxinfo" name="cpf" id="cpf" type="text" placeholder="Digite o CPF" value="{{$orientador->cpf}}" ><br/><br>
 
-            <label for="matricula" style="display:flex; font-weight: 600; font-size: 20px; line-height: 28px; color: #131833;">Matrícula: </label>
-            <input type="text" name="matricula" id="matricula" placeholder="Digite a matrícula" value="{{$orientador->matricula}}" 
-            style="background: #F5F5F5; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px;
-            box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);"><br><br>
+            <label for="matricula" class="titulo">Matrícula: </label>
+            <input class="boxinfo" type="text" name="matricula" id="matricula" placeholder="Digite a matrícula" value="{{$orientador->matricula}}"><br><br>
 
             <div style="display: flex; align-content: center; align-items: center; justify-content: center; gap:5%">
                 <input type="button" class="btn btn-secondary" value="Voltar" href="{{url("/orientadors/")}}" onclick="window.location.href='{{url("/orientadors/")}}'"
