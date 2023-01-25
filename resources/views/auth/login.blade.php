2cf59b8f39c730df9435a731cddcd1ebd0cd5c20
Anna Victoria Feliciana <anna.feliciana@upe.br>
2023-01-25 20:37:34 -0300

    Edição no botão de cadastro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,18 +11,18 @@
         </div>
     @endif --}}
     <div class="container" style="padding-top: 5%">
-    
+
         <div class="row" style="justify-content: center; align-items: center">
 
             <div class="col">
                 <p style="font-weight: 700; font-size:30px; text-align: start">O que é?</p>
                 <p style="font-weight: 400; font-size:20px; text-align: justify; padding-right: 5%; padding-top: 3%">
-                O sistema permite o gerenciamento de programas acadêmicos (bolsas, monitorias, estágios, etc..), 
-                de forma a facilitar e automatizar o processo. Com esse sistema todos os envolvidos no processo 
+                O sistema permite o gerenciamento de programas acadêmicos (bolsas, monitorias, estágios, etc..),
+                de forma a facilitar e automatizar o processo. Com esse sistema todos os envolvidos no processo
                 poderão ter mais controle da sua função, tornando o processo mais eficaz.
                 </p>
             </div>
-            
+
             <div class="col form-card">
                 <h2 style="font-style: normal; font-weight: 700; font-size: 30px; line-height: 59px;
                  color: #131833; text-align: center">Entrar</h2>
@@ -54,17 +54,17 @@
                         </div>
 
                     </div>
-                    
+
                     <div style="display:flex">
                         <!-- :<div> -->
-                        <button type="submit" class="btn btn-primary submit-button" 
+                        <button type="submit" class="btn btn-primary submit-button"
                         style="background: #34A853; height: 40px; width: 200px; border-radius: 15px;
                          margin-left: 0; margin-top: 30px; width: 30%; border: none;">Entrar</button>
                         <!-- </div> -->
                         <!-- <div style="margin-left: auto; margin-right: 0;"> -->
-                        <button type="cadastro" class="btn btn-primary submit-button" style="background: #2D3875; height: 40px;
-                         width: 200px; border-radius: 15px; margin-left: auto; margin-right: 0; margin-top: 30px;
-                          width: 30%; border: none;">Cadastre-se</button>
+                        <a href="{{url("/cadastrar-se/")}}" style="background: #2D3875; height: 40px; color:#F5F5F5;
+                        width: 200px; border-radius: 15px; margin-left: auto; margin-right: 0; margin-top: 30px;
+                        width: 30%; border: none; text-decoration: none; padding-top: 8px"> Cadastre-se</a>
                         <!-- </div> -->
 
                     <!-- <a href="#" data-bs-toggle="modal" data-bs-target="#modal_create" style="text-decoration: none; cursor: point;">Cadastre-se</a> -->
