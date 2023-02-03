f43ff94d173fa6c8ba0b6696d420aeacb638eaff
victorMendes18 <victorfran18@gmail.com>
2023-02-03 18:48:33 -0300

    Cadastro de servidor no Cadastra-se

--- app/Http/Controllers/CadastrarSeController.php
+++ app/Http/Controllers/CadastrarSeController.php
@@ -3,10 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\CadastreSeStoreFormRequest;
+use App\Models\Curso;
+use App\Models\Servidor;
+use App\Models\Tipo_servidor;
+use Illuminate\Support\Facades\Hash;
 
 class CadastrarSeController extends Controller
 {
     public function cadastrarSe(){
-        return view('CadastrarSe.cadastrarSe');
+        $cursos = Curso::all();
+        $tipo_servidores = Tipo_servidor::all();
+        return view('CadastrarSe.cadastrarSe', compact('cursos', 'tipo_servidores'));
+    }
+
+    public function store(CadastreSeStoreFormRequest $request)
+    {
+        if ($request->tipoUser == "servidor"){
+            $servidor = Servidor::Create([
+                'cpf' => $request->input('cpf'),
+                'id_tipo_servidor' => (int) $request->input('tipo_servidor')
+            ]);
+
+            if(
+                $servidor->user()->create([
+                    'name' => $request->input('nome'),
+                    'email' => $request->input('email'),
+                    'password' => Hash::make($request->input('senha'))
+                ])
+            ){
+                return redirect('/')->with('sucesso', 'Usuário cadastrado com sucesso.');
+
+            } else {
+                return redirect()->back()->withErrors( "Falha ao cadastrar Usuário. tente novamente mais tarde." );
+            }
+        } else if ($request->tipoUser == "orientador"){
+
+        } else {
+
+        }
     }
 }
