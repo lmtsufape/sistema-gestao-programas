1543af779254b3f065cc9ac0cab5ad9cb4c17c06
jacksonlmp <jacksonlmp17.jl@gmail.com>
2023-02-15 17:55:53 -0300

    Permissoes de Edital

--- resources/views/Edital/components/modal_show.blade.php
+++ resources/views/Edital/components/modal_show.blade.php
@@ -1,34 +1,39 @@
-<div class="modal fade " id="modal_show_{{$edital->id}}" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content modal-create p-3" style="border-radius: 15px; background-color: #f5f5f5; font-family: 'Roboto', sans-serif;">
-            <div class="modal-header" >
-                <h5 style="color: #131833; font-style: normal; font-weight: 600; font-size: 30px; line-height: 47px;" class="modal-title title fw-bold">Informações do Edital</h5>
-                <button class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body" style="overflow: auto">
-                <div class="mb-3" style="">
-                    <label style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833;" class="form-label mt-3">Data de início:</label>
-                    <div  style="background: #EEEEEE; border-radius: 13px; border: 1px #EEEEEE; width: 100%; padding: 5px; box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);"> {{date_format(date_create($edital->data_inicio), "d/m/Y")}}</div>
+@canany(['admin', 'servidor'])
+  <div class="modal fade " id="modal_show_{{$edital->id}}" tabindex="-1" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content modal-create p-3" style="border-radius: 15px; background-color: #f5f5f5; font-family: 'Roboto', sans-serif;">
+              <div class="modal-header" >
+                  <h5 style="color: #131833; font-style: normal; font-weight: 600; font-size: 30px; line-height: 47px;" class="modal-title title fw-bold">Informações do Edital</h5>
+                  <button class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body" style="overflow: auto">
+                  <div class="mb-3" style="">
+                      <label style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833;" class="form-label mt-3">Data de início:</label>
+                      <div  style="background: #EEEEEE; border-radius: 13px; border: 1px #EEEEEE; width: 100%; padding: 5px; box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);"> {{date_format(date_create($edital->data_inicio), "d/m/Y")}}</div>
 
-                    <label style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833;" class="form-label mt-3">Data de fim:</label>
-                    <div  style="background: #EEEEEE; border-radius: 13px; border: 1px #EEEEEE; width: 100%; padding: 5px; box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);"> {{date_format(date_create($edital->data_fim), "d/m/Y")}}</div>
+                      <label style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833;" class="form-label mt-3">Data de fim:</label>
+                      <div  style="background: #EEEEEE; border-radius: 13px; border: 1px #EEEEEE; width: 100%; padding: 5px; box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);"> {{date_format(date_create($edital->data_fim), "d/m/Y")}}</div>
 
-                    <label style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833;" class="form-label mt-3">Programa:</label>
-                    <div style="background: #EEEEEE; border-radius: 13px; border: 1px #EEEEEE; width: 100%; padding: 5px; box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);">{{$edital->programa->nome}}</div>
-                </div>
-            </div>
-        </div>
+                      <label style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833;" class="form-label mt-3">Programa:</label>
+                      <div style="background: #EEEEEE; border-radius: 13px; border: 1px #EEEEEE; width: 100%; padding: 5px; box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);">{{$edital->programa->nome}}</div>
+                  </div>
+              </div>
+          </div>
 
-    </div>
-</div>
-<style>
-  .btn-secondary{
-    color: #fff;
-    background-color: #2d3875;
-    border-color: #2d3875;
-  }
-  .btn-secondary:hover{
-    background-color: #4353ab;
-    border-color: #4353ab;
-  }
-</style>
+      </div>
+  </div>
+  <style>
+    .btn-secondary{
+      color: #fff;
+      background-color: #2d3875;
+      border-color: #2d3875;
+    }
+    .btn-secondary:hover{
+      background-color: #4353ab;
+      border-color: #4353ab;
+    }
+  </style>
+@else
+    <h3 style="margin-top: 1rem">Você não possui permissão!</h3>
+    <a class="btn btn-primary submit" style="margin-top: 1rem" href="{{url("/login")}}">Voltar</a>
+@endcan
