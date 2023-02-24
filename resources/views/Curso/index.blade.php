61bccfe261bc86c39a899b064f56a4489785b7a2
Anna Victoria Feliciana <anna.feliciana@upe.br>
2023-02-24 17:10:47 -0300

    ajuste altura cursos

--- resources/views/Curso/index.blade.php
+++ resources/views/Curso/index.blade.php
@@ -43,10 +43,8 @@
       @if (session('sucesso'))
       @endif
       <br>
-
-      <div style="margin-bottom: 10px;  gap: 20px; margin-top: 20px">
+      <div style="margin-bottom: 10px;  gap: 20px;">
         <h1><strong>Cursos</strong></h1>
-      <div style="margin: auto"></div>
       <form action="{{route("cursos.index")}}" method="GET">
         <input type="text" onkeyup="" placeholder="Digite a busca" title="" id="valor" name="valor"
         style="background-color: #D9D9D9;
@@ -93,7 +91,7 @@
       </div>
     @else
     @endif
-        
+
         <div class="d-flex flex-wrap justify-content-center" style="flex-direction: row-reverse;">
             <div class="col-md-9 corpo p-2 px-3">
              <table class="table" style="border-radius: 10px; background-color: #F2F2F2;
