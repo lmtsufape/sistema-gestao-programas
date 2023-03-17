0c798815bbec383a71fb25fbea94755cb785dfd0
Camila Claudino <camila.claudino@upe.br>
2023-03-17 15:57:53 -0300

    Adicionando ícones e rotas no index de programa

--- resources/views/Programa/index.blade.php
+++ resources/views/Programa/index.blade.php
@@ -73,7 +73,7 @@
   <div style="padding-bottom: 6px">
     <a style="background: #2D3875; border-radius: 20px; border: #2D3875; color: #f0f0f0;
     font-weight: 400; font-size: 24px; padding-top: 5px; padding-bottom: 6px; padding-right: 15px;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); text-decoration: none; padding-left: 10px;""
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); text-decoration: none; padding-left: 10px;"
         href="{{route("programas.create")}}">
         <img src="{{asset("images/plus.png")}}" alt="Cadastrar programa" style="padding-bottom: 5px"> Cadastrar programa
         </a>
@@ -102,6 +102,9 @@
                     <td class="align-middle"> {{$programas->nome}} </td>
                     <td class="align-middle"> {{$programas->descricao}} </td>
                     <td class="align-middle">
+                        <a href="{{url("/programas/$programas->id/alunos")}}">
+                            <img src="{{asset("images/bx_user.png")}}" alt="Listar alunos">
+                        </a>
                         <a href="{{url("/programas/$programas->id/editals")}}">
                             <img src="{{asset("images/listaredital.png")}}" alt="Listar edital">
                         </a>
@@ -158,6 +161,12 @@
                         <div style="display: flex; margin: 10px">
                             <a><img src="/images/listaredital.png" alt="Listar editais" style="width: 20px; height: 20px;"></a>
                             <p class="textolegenda">Listar Edital</p>
+                        </div>
+                    </div>
+                    <div style="align-self: center; margin-right: auto">
+                        <div style="display: flex; margin: 10px">
+                            <a><img src="/images/bx_user.png" alt="Listar alunos" style="width: 20px; height: 20px;"></a>
+                            <p class="textolegenda">Listar Alunos</p>
                         </div>
                     </div>
                 </div>
