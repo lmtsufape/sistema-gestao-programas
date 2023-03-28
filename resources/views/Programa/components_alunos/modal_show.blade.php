56c7bc4f42a389f6a92e7a88a4e6f102c94acf2d
Camila Claudino <camila.claudino@upe.br>
2023-03-28 18:28:03 -0300

    atualização da branch

---
+++ resources/views/Programa/components_alunos/modal_show.blade.php
@@ -0,0 +1,43 @@
+<div class="modal fade " id="modal_show_" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered"> <!-- div antes do real modal -->
+    <div class="modal-content modal-create p-3" style="border-radius: 15px; background-color: #F9F9F9; font-family: 'Roboto', sans-serif;">
+      <div class="modal-header">
+        <h5 style="color: #2D3875; font-style: normal; font-weight: 600; font-size: 30px; line-height: 47px; text-align: center;" class="modal-title title fw-bold">Informações do Aluno</h5>
+        <button class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body" style="overflow: auto">
+        <div class="mb-3">
+          <label style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833; margin-bottom:8px;" class="form-label mt-3">Nome:</label>
+          <div style="background: #EEEEEE; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px"> </div>
+
+          <label style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833; margin-bottom:8px;" class="form-label mt-3">E-mail:</label>
+          <div style="background: #EEEEEE; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px"> </div>
+
+          <label style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833; margin-bottom:8px;" class="form-label mt-3">CPF:</label>
+          <div style="background: #EEEEEE; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px"></div>
+          
+          <label style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833; margin-bottom:8px;" class="form-label mt-3">Curso:</label>
+          <div style="background: #EEEEEE; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px"> </div>
+
+          <label style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833; margin-bottom:8px;" class="form-label mt-3">Semestre de Entrada:</label>
+          <div style="background: #EEEEEE; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px"></div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" style="background: #34A853; border: #34A853;" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<style>
+  .btn-secondary {
+    color: #fff;
+    background-color: #2d3875;
+    border-color: #2d3875;
+  }
+
+  .btn-secondary:hover {
+    background-color: #4353ab;
+    border-color: #4353ab;
+  }
+</style>
