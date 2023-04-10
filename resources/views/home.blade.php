fc13e1be35087f74b28fca1fdc80af3eff439bcf
Camila Claudino <camila.claudino@upe.br>
2023-04-10 19:08:42 -0300

    Criação da rota

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -105,53 +105,53 @@
         <div class="container">
             <div>
 
-            <h1
-            style="font-style: normal; padding-top: 38px;
-            font-weight: 700; text-align:start ;
-            font-size: 35px; line-height: 41px; color: #131833;">
-            Bem-vindo(a)!
-            </h1>
-            <hr>
-
+                <h1
+                style="font-style: normal; padding-top: 38px;
+                font-weight: 700; text-align:start ;
+                font-size: 35px; line-height: 41px; color: #131833;">
+                Bem-vindo(a)!
+                </h1>
+                <hr>
+                <br>
 
             </div>
 
             <div style="display: flex; gap: 5%; align-items: center; margin-top: 1% ; margin-bottom: 1% ; margin-left: 2%">
 
-            {{--  <button class="botaoverde">
-                <img src="{{asset("images/DocumentAdd.png")}}" alt="logodoc" style="padding-right: 10px;">
-                <p style="margin: auto; padding-right: 5px"> Listar documentos </p>
-            </button>  --}}
+                <!-- <button class="botaoverde">
+                    <img src="{{asset("images/DocumentAdd.png")}}" alt="logodoc" style="padding-right: 10px;">
+                    <p style="margin: auto; padding-right: 5px"> Listar documentos </p>
+                </button>   -->
 
-            <button class="botaoverde" ref="{{url("/listar-modelos")}}" onclick="window.location.href='{{url("/listar-modelos")}}'">
-                <img src="{{asset("images/DocumentAdd.png")}}" alt="logodoc" style="padding-right: 10px;">
-                <p style="margin: auto; padding-right: 5px"> Listar modelos de documentos </p>
-            </button>
+                <button class="botaoverde" ref="{{url("/listar-modelos")}}" onclick="window.location.href='{{url("/listar-modelos")}}'">
+                    <img src="{{asset("images/DocumentAdd.png")}}" alt="logodoc" style="padding-right: 10px;">
+                    <p style="margin: auto; padding-right: 5px"> Listar modelos de documentos </p>
+                </button>
 
-            {{--  <button class="botaoazul">
-                <img src="{{asset("images/documentoadicionaricon.png")}}" alt="logodoc" style="padding-right: 10px;">
-                <p style="margin: auto; padding-right: 5px"> Gerar documentos </p>
-            </button>  --}}
-{{--
-            <button class="botaoverde">
-                <img src="{{asset("images/certificadoicon.png")}}" alt="logodoc" style="padding-right: 10px;">
-                <p style="margin: auto; padding-right: 5px"> Ver meus certificados </p>
-            </button>  --}}
+                <!-- <button class="botaoazul">
+                    <img src="{{asset("images/documentoadicionaricon.png")}}" alt="logodoc" style="padding-right: 10px;">
+                    <p style="margin: auto; padding-right: 5px"> Gerar documentos </p>
+                </button>   -->
+    <!-- 
+                <button class="botaoverde">
+                    <img src="{{asset("images/certificadoicon.png")}}" alt="logodoc" style="padding-right: 10px;">
+                    <p style="margin: auto; padding-right: 5px"> Ver meus certificados </p>
+                </button>  -->
 
-            <button class="botaoazul">
-                <img src="{{asset("images/programaicon.png")}}" alt="logodoc" style="padding-right: 10px;">
-                <p style="margin: auto; padding-right: 5px"> Ver meus programas </p>
-            </button>
+                <button class="botaoazul">
+                    <img src="{{asset("images/programaicon.png")}}" alt="logodoc" style="padding-right: 10px;">
+                    <p style="margin: auto; padding-right: 5px"> Ver meus programas </p>
+                </button>
 
 
             </div>
 
-            {{--  <div style="display: flex; gap: 5%; align-items: center; margin-top: 1% ; margin-bottom: 1% ; margin-left: 2%">
+            <!--  <div style="display: flex; gap: 5%; align-items: center; margin-top: 1% ; margin-bottom: 1% ; margin-left: 2%">
             <button class="botaoverde" ref="{{url("/listar-modelos")}}" onclick="window.location.href='{{url("/listar-modelos")}}'">
                 <img src="{{asset("images/DocumentAdd.png")}}" alt="logodoc" style="padding-right: 10px;">
                 <p style="margin: auto; padding-right: 5px"> Listar modelos de documentos </p>
             </button>
-            </div>  --}}
+            </div>  -->
 
         </div>
 
@@ -171,13 +171,13 @@
             Bem vindo(a)!
             </h1>
             <hr>
-
+            <br>
 
             </div>
 
             <div style="display: flex; gap: 5%; align-items: center; margin: auto;">
 
-                <button class="botaoverde" href="{{url("/alunos/")}}" onclick="window.location.href='{{url("/alunos/")}}'">
+                <button class="botaoverde" href="{{url("/MeusAlunos")}}" onclick="window.location.href='{{url("/MeusAlunos")}}'">
                     <img src="{{asset("images/user.png")}}" alt="user" style="padding-right: 20px;">
                     <p style="margin: auto; padding-right: 5px">Listar alunos </p>
                 </button>
@@ -187,30 +187,31 @@
                     <p style="margin: auto; padding-right: 5px"> Meus programas </p>
                 </button>
 
-                {{--  <button class="botaoverde">
+                <!-- <button class="botaoverde">
                     <img src="{{asset("images/certificadoicon.png")}}" alt="logodoc" style="padding-right: 20px;">
                     <p style="margin: auto; padding-right: 5px"> Meus certificados </p>
-                </button>  --}}
+                </button>  
 
-                {{--  <button class="botaoazul">
+                <button class="botaoazul">
                     <img src="{{asset("images/programaicon.png")}}" alt="logodoc" style="padding-right: 20px;">
                     <p style="margin: auto; padding-right: 5px">  Visualizar documentos </p>
-                </button>  --}}
-
+                </button>   -->
+                <!-- 
                 <button class="botaoverde">
                     <img src="{{asset("images/calendar.png")}}" alt="calendario" style="padding-right: 20px;">
                     <p style="margin: auto; padding-right: 10px"> Visualizar frequência mensal </p>
                 </button>
-            </div>
+                -->
+            </div> 
 
-            <br>
+            <!-- <br>
 
-            {{--  <div style="display: flex; gap: 5%; align-items: center; margin: auto;">
+            <div style="display: flex; gap: 5%; align-items: center; margin: auto;">
                 <button class="botaoverde">
                     <img src="{{asset("images/calendar.png")}}" alt="calendario" style="padding-right: 20px;">
                     <p style="margin: auto; padding-right: 10px"> Visualizar frequência mensal </p>
                 </button>
-            </div>  --}}
+            </div>  -->
 
         </div>
 
