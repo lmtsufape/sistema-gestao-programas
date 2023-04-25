b36651774aebed6c8276cff44b1562a4ddd572fe
Ines <ines.alessandra03@gmail.com>
2023-04-25 18:20:38 -0300

    salvando e enviando para view nome social

--- resources/views/Alunos/cadastro-aluno.blade.php
+++ resources/views/Alunos/cadastro-aluno.blade.php
@@ -80,7 +80,7 @@
             <div class="invalid-feedback"> Por favor preencha esse campo</div><br><br>
 
             <label for="inputNomeSocial" class="titulo">Nome Social:</label>
-            <input class="boxinfo" type="text" id="inputNomeSocial" name="nome_social" required placeholder="Digite o nome social">
+            <input class="boxinfo" type="text" id="inputNomeSocial" name="nome_social" placeholder="Digite o nome">
             <div class="invalid-feedback"> Por favor preencha esse campo</div><br><br>
 
             <label for="inputCpf" class="titulo">CPF:</label>
