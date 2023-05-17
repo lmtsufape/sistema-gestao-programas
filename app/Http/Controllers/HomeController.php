7b9bd3fb6818a51cd4522465c32418fe96107af8
Ines <ines.alessandra03@gmail.com>
2023-05-17 17:08:26 -0300

    listar programas na tela inicial

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,12 +9,7 @@ use App\Models\Edital;
 class HomeController extends Controller {
 
     public function index() {
-<<<<<<< HEAD
         $programas = Programa::all();
         return view('home', compact('programas'));
-=======
-        $editais = Edital::all();
-        return view('home', compact('editais'));
->>>>>>> refs/remotes/origin/main
     }
 }
