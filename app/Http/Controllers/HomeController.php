a49696790315adf6bdedd8281d687541db41f328
Camila Claudino <camila.claudino@upe.br>
2023-05-17 14:54:53 -0300

    Correção da home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Edital;
 
 class HomeController extends Controller {
 
     public function index() {
-        return view('home');
+        $editais = Edital::all();
+        return view('home', compact('editais'));
     }
 }
