43d7ff12523595d33086f2aef12aa65a6074e481
Camila Claudino <camila.claudino@upe.br>
2023-05-21 21:52:27 -0300

    criação da tela de listar orientadores

--- resources/views/Edital/components/modal_show.blade.php
+++ resources/views/Edital/components/modal_show.blade.php
@@ -10,16 +10,16 @@
         <div class="mb-3">
 
           <label style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833; margin-bottom:8px;" class="form-label mt-3">Título:</label>
-          <div style="background: #EEEEEE; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px">{{$edital->titulo_edital}} </div>
+          <div style="background: #EEEEEE; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px"> {{$edital->titulo_edital}} </div>
 
           <label style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833; margin-bottom:8px;" class="form-label mt-3">Semestre:</label>
-          <div style="background: #EEEEEE; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px">{{$edital->semestre}} </div>
+          <div style="background: #EEEEEE; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px"> {{$edital->semestre}} </div>
 
           <label style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833; margin-bottom:8px;" class="form-label mt-3">Descrição:</label>
-          <div style="background: #EEEEEE; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px">{{$edital->descricao}} </div>
+          <div style="background: #EEEEEE; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px"> {{$edital->descricao}} </div>
           
           <label style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833; margin-bottom:8px;" class="form-label mt-3">Valor da Bolsa:</label>
-          <div style="background: #EEEEEE; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px">R$ {{$edital->valor_bolsa}},00 </div>
+          <div style="background: #EEEEEE; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px"> {{$edital->valor_bolsa}} </div>
 
           <label style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833; margin-bottom:8px;" class="form-label mt-3">Data de início:</label>
           <div style="background: #EEEEEE; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px"> {{date_format(date_create($edital->data_inicio), "d/m/Y")}}</div>
@@ -28,10 +28,10 @@
           <div style="background: #EEEEEE; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px"> {{date_format(date_create($edital->data_fim), "d/m/Y")}}</div>
 
           <label style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833; margin-bottom:8px;" class="form-label mt-3">Programa:</label>
-          <div style="background: #EEEEEE; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px">{{$edital->programa->nome}}</div>
+          <div style="background: #EEEEEE; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px"> {{$edital->programa->nome}} </div>
 
           <label style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833; margin-bottom:8px;" class="form-label mt-3">Disciplina:</label>
-          <div style="background: #EEEEEE; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px">{{$edital->disciplina->nome}}</div>
+          <div style="background: #EEEEEE; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px"> {{$edital->disciplina->nome}} </div>
           
           <label style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833; margin-bottom:8px;" class="form-label mt-3">Alunos:</label> 
           <div style=" display:flex; flex-wrap:wrap; justify-content:center; align-items:center;">
@@ -41,6 +41,16 @@
             </a> 
           </div> 
         </div>
+
+        <label style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833; margin-bottom:8px;" class="form-label mt-3">Orientador:</label>
+        <div style=" display:flex; flex-wrap:wrap; justify-content:center; align-items:center;">
+          <a class="link" alt="Listar orientadores" href="{{  route('edital.listar_orientadores', ['edital_id' => $edital->id]) }}" style="background: #EEEEEE; border-radius: 13px; border: 1px #D3D3D3;width: 100%; display:flex; flex-direction:row; flex-wrap:wrap; justify-content:center; align-items:center; padding:5px; margin:2px;">
+            <img src="{{asset("images/bx_user.png")}}" >
+            Listar orientadores
+          </a> <br>
+        </div> 
+        
+
         <div class="modal-footer">
           <button type="button"  class="btn" data-bs-dismiss="modal">Fechar</button>
         </div>
---
+++ resources/views/Edital/listar_orientadores.blade.php
@@ -0,0 +1,152 @@
+@extends("templates.app")
+
+@section("body")
+
+<style>
+  pagination {
+    display: inline-block;
+
+  }
+
+  .pagination a {
+    color: black;
+    float: left;
+    padding: 8px 16px;
+    text-decoration: none;
+    transition: background-color .3s;
+    border: 1px solid #ddd;
+    margin: 10px 4px;
+  }
+
+  .pagination a.active {
+    background-color: #3B864F;
+    color: white;
+    border: 1px solid #3B864F;
+  }
+
+  .pagination a:hover:not(.active) {
+    background-color: #34A853;
+  }
+</style>
+
+<div class="container">
+  @if (session('sucesso'))
+  <div class="alert alert-success">
+    {{session('sucesso')}}
+  </div>
+  @endif
+  <br>
+
+  <div style="margin-bottom: 10px;  gap: 20px; margin-top: 20px">
+    <h1 style="color:#2D3875;"><strong>Orientadores Vinculados</strong></h1>
+    <div style="margin: auto"></div>
+    <form action="" method="GET">
+      <input class="text-center p-3" type="text" onkeyup="" placeholder="Digite a busca" title="" id="valor" name="valor" style="background-color: #D9D9D9;
+                  border-radius: 30px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+                  background-position: 10px 2px;
+                  background-repeat: no-repeat;
+                  width: 35%;
+                  font-size: 16px;
+                  height: 45px;
+                  border: 1px solid #ddd;
+                  margin-bottom: 12px; margin-right: 10px
+                  ">
+
+      <input type="submit" value="" style="background-image: url('/images/searchicon.png');
+                  background-color: #D9D9D9;
+                  border-radius: 30px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+                  width: 40px;
+                  font-size: 16px;
+                  height: 45px;
+                  border: 1px solid #ddd;
+                  position: absolute;
+                  margin: auto;" />
+
+    </form>
+  </div>
+ 
+ <br><br>
+  <div class="d-flex flex-wrap justify-content-center" style="flex-direction: row-reverse;">
+    <div class="col-md-9 corpo p-2 px-3">
+      <table class="table" style="border-radius: 15px; background-color: #F2F2F2; min-width: 600px; box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25)
+          ;margin-bottom: 5px; min-height: 350px">
+        <thead>
+          <tr>
+            <th scope="col">Nome</th>
+            <th scope="col">Edital</th>
+            <th scope="col">Data de Início</th>
+            <th scope="col">Data de Fim</th>
+            <th class="text-center">Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+        @foreach($orientadores as $orientador) 
+          @foreach ($pivot as $pivo)
+          <tr>
+            <td> {{ $orientador->name}} </td>
+            <td> {{ $pivo->titulo }} </td>
+            <td> {{ date('d/m/Y', strtotime($pivo->data_inicio)) }} </td>
+            <td> {{ date('d/m/Y', strtotime($pivo->data_fim)) }} </td>
+            <td>
+
+              {{--<a type="button" data-bs-toggle="modal" data-bs-target="#modal_delete{{$pivo->edital_id}}">
+                <img src="{{asset("images/delete.png")}}" alt="Deletar orientadores" style="height: 30px; width: 30px;">
+              </a>--}}
+              <a type="button" data-bs-toggle="modal" data-bs-target="#modal_documents{{$pivo->edital_id}}">
+                <img src="{{asset('images/document.png')}}" alt="Documento orientadores"  style="height: 30px; width: 30px;">
+              </a>
+            </td>
+          </tr>
+          @include('Edital.components_orientadores.modal_documents', ['pivo' => $pivo])
+          @include('Edital.components_orientadores.modal_delete', ['pivo' => $pivo, 'orientador' => $orientador])
+          @endforeach
+        @endforeach
+        </tbody>
+      </table>
+    </div>
+
+    <div style="background-color: #F2F2F2; border-radius: 15px; justify-content: center; align-items: center
+            ; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); width: 150px; height: 40%;">
+
+      <div style="align-self: center; margin-right: auto">
+        <br>
+        <h4 class="fw-bold" style="font-size: 15px; color:#2D3875;">Legenda dos ícones:</h4>
+      </div>
+      <div style="align-self: center; margin-right: auto">
+        <div style="display: flex; margin: 10px">
+          <a><img src="{{asset("images/searchicon.png")}}" alt="Procurar" style="width: 20px; height: 20px;"></a>
+          <p style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%; margin:5px">Pesquisar</p>
+        </div>
+        <div style="display: flex; margin: 10px">
+          <a><img src="/images/document.png" alt="Documentos" style="width: 20px; height: 20px;"></a>
+          <p style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%; margin:5px">Documentos</p>
+        </div>
+      </div>
+      <div style="align-self: center; margin-right: auto">
+        <div style="display: flex; margin: 10px">
+          <a><img src="{{asset("images/delete.png")}}" alt="Deletar orientador" style="width: 20px; height: 20px;"></a>
+          <p style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%; margin:5px">Deletar</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br>
+  <br>
+</div>
+
+<script type="text/javascript">
+  function exibirModalDeletar(id) {
+    $('#modal_delete_' + id).modal('delete');
+  }
+
+  function exibirModalVisualizar(id) {
+    $('#modal_show_' + id).modal('show');
+  }
+
+  function exibirModalDocumentos(id) {
+    $('#modal_documents' + id).modal('documents');
+  }
+</script>
+
+
+@endsection
