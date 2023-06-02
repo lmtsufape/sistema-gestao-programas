ea1fb34d86371f87936c4a6f971fa6a9cfebe882
Camila Claudino <camila.claudinoo123@gmail.com>
2023-06-01 23:49:14 -0300

    correção nas telas de perfil

--- resources/views/Perfil/meu-perfil-orientador.blade.php
+++ resources/views/Perfil/meu-perfil-orientador.blade.php
@@ -1,7 +1,7 @@
 @extends("templates.app")
 
 @section("body")
-
+<div class="container" style="width: 70%; background: #FBFBFB; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 10px; padding: 10px 40px 30px 40px; margin-top:2rem;">
     <div class="container">
         <div style="margin-bottom: 10px;  gap: 20px; margin-top: 20px">
             <h1 style="color:#2D3875;"><strong>Meu Perfil</strong></h1>
@@ -28,10 +28,12 @@
 
             <label style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833; margin-bottom:8px;" class=" form-label mt-3">Matrícula:</label>
             <div style="background: #EEEEEE; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);"> {{$orientador->matricula}}</div>
-
         </div>
+        <br>
+        <br>
         @endif
     @endauth
-
-
+</div>
+<br>
+<br>
 @endsection
--- resources/views/Perfil/meu-perfil-servidor.blade.php
+++ resources/views/Perfil/meu-perfil-servidor.blade.php
@@ -2,6 +2,7 @@
 
 @section("body")
 
+<div class="container" style="width: 70%; background: #FBFBFB; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 10px; padding: 10px 40px 30px 40px; margin-top:2rem;">
     <div class="container">
         <div style="margin-bottom: 10px;  gap: 20px; margin-top: 20px">
             <h1 style="color:#2D3875;"><strong>Meu Perfil</strong></h1>
@@ -9,7 +10,7 @@
     </div>
 
     @auth
-        <div class="container">
+        <div class="container" >
             <label style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833; margin-bottom:8px;" class=" form-label mt-3">Nome:</label>
             <div style="background: #EEEEEE; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);"> {{$servidor->user->name}} </div>
 
@@ -27,6 +28,10 @@
             <label style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833; margin-bottom:8px;" class=" form-label mt-3">Tipo do servidor:</label>
             <div style="background: #EEEEEE; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);"> {{$servidor->tipo_servidor}} </div>
         </div>
+        <br>
+        <br>
     @endauth
-
+</div>
+<br>
+<br>
 @endsection
