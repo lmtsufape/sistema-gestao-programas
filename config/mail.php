74119ab1e9689bbea27094d9b724b31a47afcc4b
Itamar Bernardo <itamarbernardo2013@gmail.com>
2023-06-05 15:10:54 -0300

    Acrescentar o nome da UFAPE no texto do E-mail de recuperar senha e modificado o nome da aplicacao no email

--- config/mail.php
+++ config/mail.php
@@ -93,7 +93,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => 'Sistema Gestão de Programas',
+        'name' => 'Sistema de Gestão de Programas Acadêmicos',
     ],
 
     /*
