85bd6162dcb7570b0467a9f8bf9c4e30dbbe4f4a
Ines <ines.alessandra03@gmail.com>
2023-06-08 01:15:29 -0300

    add mensagem de orientadores não cadastrados em edital

--- app/Http/Controllers/EditalController.php
+++ app/Http/Controllers/EditalController.php
@@ -256,7 +256,7 @@ class EditalController extends Controller
         $pivot = EditalAlunoOrientadors::where('edital_id', $id)->get();
         $count = $pivot->count();
         if($pivot->isEmpty()) {
-            return redirect()->back()->with('fail', 'Não há orientadores cadastrados no edital.');
+            return redirect()->back()->with('falha', 'Não há orientadores cadastrados no edital.');
         }
         elseif($count > 1) {
             foreach($pivot as $pivo) {
@@ -345,4 +345,3 @@ class EditalController extends Controller
 
 
 }
-
