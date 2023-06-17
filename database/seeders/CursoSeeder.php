801c7f191df85514762c3d262540c8d71984b03e
Camila Claudino <camila.claudinoo123@gmail.com>
2023-06-17 16:03:58 -0300

    adicionando os cursos no seeder

--- database/seeders/CursoSeeder.php
+++ database/seeders/CursoSeeder.php
@@ -15,11 +15,35 @@ class CursoSeeder extends Seeder
     public function run()
     {
         $curso = Curso::create([
-            'nome' => "BCC"
+            'nome' => "Bacharelado em Agronomia"
         ]);
 
         $curso2 = Curso::create([
-            'nome' => "Veterinaria"
+            'nome' => "Bacharelado em Ciência da computação"
+        ]);
+
+        $curso3 = Curso::create([
+            'nome' => "Bacharelado em Engenharia de Alimentos"
+        ]);
+
+        $curso4 = Curso::create([
+            'nome' => "Bacharelado em Medicina Veterinária"
+        ]);
+
+        $curso5 = Curso::create([
+            'nome' => "Bacharelado em Zootecnia"
+        ]);
+
+        $curso6 = Curso::create([
+            'nome' => "Licenciatura em Letras"
+        ]);
+
+        $curso7 = Curso::create([
+            'nome' => "Licenciatura em Pedagogia"
+        ]);
+
+        $curso8 = Curso::create([
+            'nome' => "Outro Curso"
         ]);
     }
 }
