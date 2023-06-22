22af2a630f342160618d39628578671fed490fc3
Anna Victoria Feliciana <anna.feliciana@upe.br>
2023-06-21 22:01:41 -0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -170,7 +170,7 @@ Route::get('/meu-perfil-servidor', [App\Http\Controllers\ServidorController::cla
 //Rota de meu perfil aluno
 Route::get('/meu-perfil-aluno', [App\Http\Controllers\AlunoController::class, 'profile'])->name('meu-perfil-aluno');
 //Editar perfil aluno
-Route::get('/meu-perfil-aluno/editar', [App\Http\Controllers\UserController::class, 'updateAluno'])->name('meu-perfil-aluno.editar');
+Route::put('/meu-perfil-aluno/editar', [App\Http\Controllers\UserController::class, 'updateAluno'])->name('meu-perfil-aluno.editar');
 //Rota de meu perfil orientador
 Route::get('/meu-perfil-orientador', [App\Http\Controllers\OrientadorController::class, 'profile'])->name('meu-perfil-orientador');
 //
