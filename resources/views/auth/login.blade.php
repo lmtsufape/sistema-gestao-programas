1ecd3b4b25a4607fb989525c067835062fd4ce54
Camila Claudino <camila.claudinoo123@gmail.com>
2023-07-14 16:36:02 -0300

    correção

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,82 @@
 @extends("templates.app")
 
+
+@section("body")
+    @include("auth.modal_criar_aluno")
+
+    {{-- <x-jet-validation-errors class="mb-4" />
+    @if (session('status'))
+        <div class="">
+            {{ session('status') }}
+        </div>
+    @endif --}}
+    @if (session('sucesso'))
+    <div class="alert alert-sucess">
+        <p style="color: green">{{session('sucesso')}}</p>
+    </div>
+    @endif
+    <br>
+
+    <div class="container" style="padding-top: 5%">
+
+        <div class="row" style="justify-content: center; align-items: center">
+
+            <div class="col div-paragraph">
+                <h2 class="title-login">O que é?</h2>
+                <p class="paragraph">
+                O sistema permite o gerenciamento de programas acadêmicos (bolsas, monitorias, estágios, etc..),
+                de forma a facilitar e automatizar o processo. Com esse sistema todos os envolvidos no processo
+                poderão ter mais controle da sua função, tornando o processo mais eficaz.
+                </p>
+            </div>
+
+            <div class="col form-card">
+                <div class="form-content">
+                    <h2 class="title-form">Entrar</h2>
+                    <hr class="divisor">
+                </div>
+                <br>
+                <form class="form-content" method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="form-body">
+                        <div class="field" >
+                            <label for="email" class="label-login">E-mail:</label>
+                            <input id="email" name="email" class="input-login form-control input-modal-create" type="text"  >
+                        </div>
+
+                        <div class="field" >
+                            <label for="password" class="label-login">Senha:</label>
+                            <input id="password" name="password" class="input-login form-control input-modal-create" type="password"  >
+                        </div>
+
+                        <div class="forgot">
+                            <a class="forgot-link" href="{{ route('password.request')}}">Esqueceu sua senha?</a>
+                        </div>
+                        
+                        <br>
+
+                        <div class="form-buttons">
+                            <!-- :<div> -->
+                            <button type="submit" class="red-button">Entrar</button>
+                            <!-- </div> -->
+                            <!-- <div style="margin-left: auto; margin-right: 0;"> -->
+                            <a href="{{ route('register')}}" class="register-button"> Cadastre-se</a>
+                            <!-- </div> -->
+
+                        <!-- <a href="#" data-bs-toggle="modal" data-bs-target="#modal_create" style="text-decoration: none; cursor: point;">Cadastre-se</a> -->
+                        </div>
+                        
+                    </div>
+                </form>
+            </div>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+    </div>
+@endsection
+
 <style>
     .div-paragraph{
         display: inline-flex;
@@ -148,78 +225,3 @@
         transition: 0.3s;
     }
 </style>
-@section("body")
-    @include("auth.modal_criar_aluno")
-
-    {{-- <x-jet-validation-errors class="mb-4" />
-    @if (session('status'))
-        <div class="">
-            {{ session('status') }}
-        </div>
-    @endif --}}
-    @if (session('sucesso'))
-    <div class="alert alert-sucess">
-        <p style="color: green">{{session('sucesso')}}</p>
-    </div>
-    @endif
-    <br>
-
-    <div class="container" style="padding-top: 5%">
-
-        <div class="row" style="justify-content: center; align-items: center">
-
-            <div class="col div-paragraph">
-                <h2 class="title-login">O que é?</h2>
-                <p class="paragraph">
-                O sistema permite o gerenciamento de programas acadêmicos (bolsas, monitorias, estágios, etc..),
-                de forma a facilitar e automatizar o processo. Com esse sistema todos os envolvidos no processo
-                poderão ter mais controle da sua função, tornando o processo mais eficaz.
-                </p>
-            </div>
-
-            <div class="col form-card">
-                <div class="form-content">
-                    <h2 class="title-form">Entrar</h2>
-                    <hr class="divisor">
-                </div>
-                <br>
-                <form class="form-content" method="POST" action="{{ route('login') }}">
-                    @csrf
-                    <div class="form-body">
-                        <div class="field" >
-                            <label for="email" class="label-login">E-mail:</label>
-                            <input id="email" name="email" class="input-login form-control input-modal-create" type="text"  >
-                        </div>
-
-                        <div class="field" >
-                            <label for="password" class="label-login">Senha:</label>
-                            <input id="password" name="password" class="input-login form-control input-modal-create" type="password"  >
-                        </div>
-
-                        <div class="forgot">
-                            <a class="forgot-link" href="{{ route('password.request')}}">Esqueceu sua senha?</a>
-                        </div>
-                        
-                        <br>
-
-                        <div class="form-buttons">
-                            <!-- :<div> -->
-                            <button type="submit" class="red-button">Entrar</button>
-                            <!-- </div> -->
-                            <!-- <div style="margin-left: auto; margin-right: 0;"> -->
-                            <a href="{{ route('register')}}" class="register-button"> Cadastre-se</a>
-                            <!-- </div> -->
-
-                        <!-- <a href="#" data-bs-toggle="modal" data-bs-target="#modal_create" style="text-decoration: none; cursor: point;">Cadastre-se</a> -->
-                        </div>
-                        
-                    </div>
-                </form>
-            </div>
-        </div>
-        <br>
-        <br>
-        <br>
-        <br>
-    </div>
-@endsection
