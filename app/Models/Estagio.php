158d3ea5d7977f2020e0da4233ce0ab3a7b70717
jonas <jonas.henrique@upe.br>
2023-07-18 18:11:44 -0300

    criação do model Estagio

---
+++ app/Models/Estagio.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Estagio extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'descricao',
+        'data_inicio',
+        'data_fim',
+        'data_solicitacao',
+        'tipo_estagio',
+        'status'
+    ];
+
+    public function aluno()
+    {
+        return $this->belongsTo(Aluno::class, "aluno_id");
+    }
+
+    public function orientador()
+    {
+        return $this->belongsTo(Orientador::class, "orientador_id");
+    }
+
+    public function disciplina()
+    {
+        return $this->belongsTo(Disciplina::class, "disciplina_id");
+    }
+
+    public function servidor()
+    {
+        return $this->belongsTo(Servidor::class, "supervisor_id");
+    }
+
+    /*public function supervisor()
+    {
+        return 0;
+    }
+    */  
+}
