85b151bc80d98636ae775ab2b537b5ce8ea08743
jonas <jonas.henrique@upe.br>
2023-08-02 18:05:20 -0300

    feat: softdelete na tabela Estágio

--- database/migrations/2023_07_18_175150_create_estagios_table.php
+++ database/migrations/2023_07_18_175150_create_estagios_table.php
@@ -14,6 +14,7 @@ class CreateEstagiosTable extends Migration
     public function up()
     {
         Schema::create('estagios', function (Blueprint $table) {
+            $table->softDeletes();
             $table->id();
             $table->timestamps();
             $table->boolean('status')->default(false);
