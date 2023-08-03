16a495eddbd299c9688aa3e7c79fe35e4f027104
MatheusInte <matheus.interaminense.guerra@gmail.com>
2023-08-03 18:55:15 -0300

    Tela de listar part1

--- resources/views/Alunos/index.blade.php
+++ resources/views/Alunos/index.blade.php
@@ -23,6 +23,79 @@
   .pagination a:hover:not(.active) {
     background-color: #34A853;
   }
+  .titulo{  
+    color: #2D3875;
+    font-size: 24px;
+  }
+  .busca{
+    border-radius: 10px 0px 0px 10px;
+    background-color: #FFF;
+    background-position: 10px 2px;
+    background-repeat: no-repeat;
+    width: 57%;
+    font-size: 16px;
+    height: 4.8vh;
+    border: 1px solid var(--preto-p-50, #E6E6E6);
+    margin-bottom: 12px; 
+    margin-right: 10px;
+  }
+  .lupa{
+    background-image: url('/images/searchicon.png');
+    border-radius: 0px 10px 10px 0px;
+    background-color: #fff;
+    width: 2.4%;
+    font-size: 16px;
+    height: 4.8vh;
+    border: 1px solid var(--preto-p-50, #E6E6E6);
+    position: absolute;
+    margin: auto;
+  }
+  .cadastrar-buttom{
+    display: flex;
+    background: var(--green-g-200, #2B8C64); 
+    border-radius: 9px; 
+    border: 1px solid var(--green-g-200, #2B8C64); 
+    justify-content: center;
+    align-items: center;
+    text-decoration: none;
+    padding: 10px;
+    width: 15.4%;
+    height: 4.8vh;
+
+  }
+  .cadastrar{
+    color: var(--tons-claros-t-50, #FEFEFE);
+    font-style: normal;
+    font-weight: 400; 
+    font-size: 14px;
+    line-height: normal;
+    text-decoration: none;
+  }
+  .cadastrar-buttom :hover{
+    text-decoration: none;
+    color: white;
+  }
+  table{
+    border-radius: 10px; 
+    background-color: #F2F2F2;
+    min-height: 50px;
+    width: 73%;
+  }
+  th {
+      background-color: #000;
+      color: #FFFFFF;
+      padding: 10px;
+    }
+    th, td {
+      border: 0px solid #ccc;
+      padding: 10px;
+      text-align: center;
+    }
+
+    tr:nth-child(even) {
+      background-color: #fff;
+    }
+
 </style>
 
 
@@ -35,48 +108,19 @@
     @endif
     <br>
 
-    <div style="margin-bottom: 10px;  gap: 20px; margin-top: 20px">
-    <h1 style="color:#2D3875;"><strong>Estudantes</strong></h1>
-    <div style="margin: auto"></div>
+    <div>
+      <h1 class = "titulo"><strong>Estudantes</strong></h1>
+    </div>
     {{--  TODO: Falta adicionar um modal com os possiveis filtros  --}}
-    <form action="{{route('alunos.index')}}" method="GET">
-      <input type="text" onkeyup="" placeholder="Digite a busca" title="" id="valor" name="valor"
-      style="background-color: #D9D9D9;
-            border-radius: 30px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-            background-position: 10px 2px;
-            background-repeat: no-repeat;
-            width: 35%;
-            font-size: 16px;
-            height: 45px;
-            border: 1px solid #ddd;
-            margin-bottom: 12px; margin-right: 10px">
-
-      <input type="submit" value=""
-      style="background-image: url('/images/searchicon.png');
-            background-color: #D9D9D9;
-            border-radius: 30px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-            width: 40px;
-            font-size: 16px;
-            height: 45px;
-            border: 1px solid #ddd;
-            position: absolute;
-            margin: auto;"
-      />
-
+    <form action="{{route('alunos.index')}}" method="GET">  
+    <input class = "busca" type="text" onkeyup="" placeholder="Digite a busca" title="" id="valor" name="valor">
+    <input class = "lupa" type="submit" value="">
     </form>
 
-  </div>
-
-
-  <div style="display: contents; align-content: center; align-items: center;">
-    <a style="background:#34A853; border-radius: 25px; border: #2D3875; color: #f0f0f0; font-style: normal;
-      font-weight: 400; font-size: 20px; line-height: 28px; padding-top: 4px; padding-bottom: 4px; align-content: center;
-      align-items: center; padding-right: 15px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); text-decoration: none;
-      padding-left: 10px;"
-    href="{{route("alunos.create")}}"
-    onmouseover="this.style.backgroundColor='#2D3875'"
-    onmouseout="this.style.backgroundColor='#34A853'">
-        <img src="{{asset("images/plus.png")}}" alt="Cadastrar estudantes" style="padding-bottom: 5px"> Cadastrar estudantes
+  <div class="cadastrar-buttom">
+    <a class="cadastrar"
+    href="{{route("alunos.create")}}">
+        Cadastrar estudantes
     </a>
     </div>
     <br>
@@ -92,8 +136,7 @@
 
     <div class="d-flex flex-wrap justify-content-center" style="flex-direction: row-reverse;">
         <div class="col-md-9 corpo p-2 px-3">
-         <table class="table" style="border-radius: 10px; background-color: #F2F2F2;
-         min-width: 600px; box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.25); min-height: 50px; ">
+         <table class="table" style=" ">
           <thead>
 
             <tr>
@@ -139,7 +182,7 @@
         </table>
 
         </div>
-
+<!--
         <div style="background-color: #F2F2F2; border-radius: 10px; margin-top: 7px; box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.25);
         width: 150px; height: 50%;">
                 <div style="align-self: center; margin-right: auto">
@@ -173,6 +216,7 @@
               <p style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%; margin:5px">Pesquisar</p>
             </div>
           </div>
+            -->
         </div>
       </div>
 
