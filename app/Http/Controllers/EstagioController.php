0a470b04e0ee6616f083f5ae1648dfa190c9ab74
Itamar Bernardo <itamarbernardo2013@gmail.com>
2023-08-04 17:39:38 -0300

    Criação da tela de Index dos estágios, corrigido nomenclatura de rota de estagio e Criação dos modais de Estágio

--- app/Http/Controllers/EstagioController.php
+++ app/Http/Controllers/EstagioController.php
@@ -3,12 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Orientador;
+use App\Models\Estagio;
+
 class EstagioController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('Estagio.index');
+        if (sizeof($request-> query()) > 0){
+            $campo = $request->query('campo');
+            $valor = $request->query('valor');
+
+            if ($valor == null){
+                return redirect()->back()->withErrors( "Deve ser informado algum valor para o filtro." );
+            }
+
+            $estagios = Estagio::where(function ($query) use ($valor) {
+                if ($valor) {
+                    $query->orWhere("users.name", "LIKE", "%{$valor}%");
+                    $query->orWhere("users.email", "LIKE", "%{$valor}%");
+                    $query->orWhere("alunos.cpf", "LIKE", "%{$valor}%");
+                    $query->orWhere("orientadors.cpf", "LIKE", "%{$valor}%");
+                    $query->orWhere("orientadors.matricula", "LIKE", "%{$valor}%");
+                    $query->orWhere("estagios.descricao", "LIKE", "%{$valor}%");
+                }
+            })->orderBy('estagios.created_at', 'desc')->select("estagios.*")->distinct()->get();
+
+            return view('Estagio.index', compact('estagios'));
+        } else {
+            $estagios = Estagio::all();
+            return view('Estagio.index', compact('estagios'));
+        }
+
     }
 
     public function create()
