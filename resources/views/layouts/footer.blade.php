8a351812c86db22dd82b82e2cad2d47d6007e034
Camila Claudino <camila.claudinoo123@gmail.com>
2023-08-06 10:24:34 -0300

    correção de conflitos

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 @section('css')
-    <!-- <link rel="stylesheet" href="/css/app.css"> -->
-    <link rel="stylesheet" href="../../../css/footer.css">
+<!-- <link rel="stylesheet" href="/css/app.css"> -->
+<link rel="stylesheet" href="../../../css/footer.css">
 @endsection
 
 <footer class="footer">
@@ -9,7 +9,7 @@
             <!-- Logos à esquerda -->
             <div class="col-3" style="margin-top: 20px; padding-left: 100px">
                 <div class="d-flex align-items-center justify-content-start">
-                    <a href="http://ufape.edu.br/" target="_blank" style=>
+                    <a href="http://ufape.edu.br/" target="_blank">
                         <img src="{{asset('images/sgpa-branco 1.svg')}}" alt="Logo SGPA" style="margin-left: 20px">
                     </a>
                 </div>
@@ -17,22 +17,22 @@
 
             <!-- Logos do centro -->
             <div class="col-6 d-flex justify-content-center" style="margin-top: 14px">
-                <div class="d-flex justify-content-around">
-                    <a href="http://ufape.edu.br/" target="_blank" style="padding-right:20px">
-                        <img src="{{ asset('images/logo_ufape_vertical.png') }}" alt="Logo UFAPE" class="logo-box" style="height:70px; width: 95px">
+                <div class="d-flex justify-content-around" style="align-self: center; gap: 5%">
+                    <a href="http://ufape.edu.br/" target="_blank">
+                        <img src="{{ asset('images/logo_ufape_vertical.png') }}" alt="Logo UFAPE" class="logo-box" style="height: 49.813px; width: 200.545px,flex-shrink 0; border-radius: 10px;">
                     </a>
-                    <a href="http://lmts.uag.ufrpe.br/" target="_blank" style="padding-right:20px">
-                        <img src="{{ asset('images/logo_ufape_color.png') }}" alt="Logo LMTS" class="logo-box" style="height:70px; width: 95px">
+                    <a href="http://lmts.uag.ufrpe.br/" target="_blank">
+                        <img src="{{ asset('images/logo_ufape_color.png') }}" alt="Logo LMTS" class="logo-box" style="height: 49.813px; width: 200.545px,flex-shrink 0; border-radius: 10px;">
                     </a>
                     <a href="https://upe.br/" target="_blank">
-                        <img src="{{ asset('images/logoupe.png') }}" alt="Logo UPE" class="logo-box" style="height:70px; width: 95px">
+                        <img src="{{ asset('images/logoupe.png') }}" alt="Logo UPE" class="logo-box" style="height: 49.813px; width: 200.545px,flex-shrink 0 ; border-radius: 10px">
                     </a>
                 </div>
             </div>
 
             <!-- Logos à direita -->
-            <div class="col-3">
-                <div class="d-flex align-items-center justify-content-end" style="margin-top: 30px; padding-right: 100px">
+            <div class="col-3 d-flex justify-content-center">
+                <div class="d-flex justify-content-around" style="align-self: center; gap: 5%">
                     <a href="https://www.facebook.com/LMTSUFAPE/" target="_blank">
                         <img src="{{asset('images/logo_facebook_branco.svg')}}" alt="Logo Facebook" style="height: 40px; padding-right:20px">
                     </a>
