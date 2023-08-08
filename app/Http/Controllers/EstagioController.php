01129beb378e4e6d6ab7fc8945c2afc9f2ff16d1
Itamar Bernardo <itamarbernardo2013@gmail.com>
2023-08-08 14:14:29 -0300

    Fix Pesquisa de estágio no Index

--- app/Http/Controllers/EstagioController.php
+++ app/Http/Controllers/EstagioController.php
@@ -22,16 +22,23 @@ class EstagioController extends Controller
                 return redirect()->back()->withErrors( "Deve ser informado algum valor para o filtro." );
             }
 
-            $estagios = Estagio::where(function ($query) use ($valor) {
+            $estagios = Estagio::join('alunos', 'estagios.aluno_id', '=', 'alunos.id')
+            ->leftJoin('orientadors', 'estagios.orientador_id', '=', 'orientadors.id')
+            ->leftJoin('users', 'users.typage_id', '=', 'orientadors.id');
+            
+            $estagios = $estagios->where(function ($query) use ($valor) {
                 if ($valor) {
+                    $query->orWhere("users.cpf", "LIKE", "%{$valor}%");
                     $query->orWhere("users.name", "LIKE", "%{$valor}%");
                     $query->orWhere("users.email", "LIKE", "%{$valor}%");
-                    $query->orWhere("alunos.cpf", "LIKE", "%{$valor}%");
-                    $query->orWhere("orientadors.cpf", "LIKE", "%{$valor}%");
                     $query->orWhere("orientadors.matricula", "LIKE", "%{$valor}%");
                     $query->orWhere("estagios.descricao", "LIKE", "%{$valor}%");
                 }
-            })->orderBy('estagios.created_at', 'desc')->select("estagios.*")->distinct()->get();
+            })
+            ->orderBy('estagios.created_at', 'desc')
+            ->select("estagios.*")
+            ->distinct()
+            ->get();
 
             return view('Estagio.index', compact('estagios'));
         } else {
