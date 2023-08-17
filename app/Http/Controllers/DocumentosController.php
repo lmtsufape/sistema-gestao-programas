3e8dc9abb5ee946a0c2b51b6e8890b93a20ffee8
jonas <jonas.henrique@upe.br>
2023-08-17 00:35:22 -0300

    feat: formulario para preenchimento do documento digitalmente

--- app/Http/Controllers/DocumentosController.php
+++ app/Http/Controllers/DocumentosController.php
@@ -3,10 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
 
 class DocumentosController extends Controller
 {
-    public function preencherDocumento() {
-        
+    public function termo_encaminhamento_form()
+    {
+        return view('Estagio.formularios.termo_encaminhamento');
+    }
+
+    public function termo_encaminhamento(Request $request)
+    {
+        $pdf = new PDFController;
+        $dados = [
+            'nome' => $request->input('nome'),
+            'curso' => $request->input('curso'),
+            'periodo' => $request->input('periodo'),
+        ];
+
+        return $pdf->editImage('termo_encaminhamento', $dados);
     }
 }
--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -6,41 +6,57 @@ use Intervention\Image\Facades\Image;
 
 class PDFController extends Controller
 {
-    public function editImage($documentPath, $documentType)
+    public function editImage($documentType, $dados)
     {
-        $documentPath = storage_path('app/docs/termo_compromisso/0.png');
-        $documentType = 'termo_compromisso';
+        
+        $documentType = 'termo_encaminhamento';
 
         // terá um método para cada documento, esse switchcase servirá para selecionar o método especifico de cada documento.
         switch ($documentType) {
-            case 'termo_compromisso':
-                return $this->editTermoCompromisso($documentPath);
+            case 'termo_encaminhamento':
+                $documentPath = storage_path('app/docs/termo_encaminhamento/0.png');
+                return $this->editTermoCompromisso($documentPath, $dados);
                 break;
             default:
                 return redirect()->back()->with('error', 'Tipo de documento desconhecido.');
         }
     }
 
-    private function editTermoCompromisso($documentPath)
+    private function editTermoCompromisso($documentPath, $dados)
     {
         $image = Image::make($documentPath);
+        
+        /*$dados[0] = 'Universidade de Pernambuco';
 
-        $image->text('Seu Texto Aqui', 100, 100, function ($font) {
+        $image->text($dados[], 280, 695, function ($font) {
             $font->file(resource_path('fonts/Arial.ttf'));
-            $font->size(36);
-            $font->color('#FFFFFF');
-            $font->align('center');
-            $font->valign('top');
+            $font->size(42);
+            $font->color('#00009C');
+
+        }); */
+
+        $image->text($dados['nome'], 280, 1060, function ($font) {
+            $font->file(resource_path('fonts/Arial.ttf'));
+            $font->size(42);
+            $font->color('#00009C');
+
         });
 
-        $newDocumentPath = storage_path('app/pdf_images/termo.png');
-        $image->save($newDocumentPath);
+        $image->text($dados['periodo'], 700, 1153, function ($font) {
+            $font->file(resource_path('fonts/Arial.ttf'));
+            $font->size(42);
+            $font->color('#00009C');
 
-        $headers = [
-            'Content-Type' => 'image/png',
-        ];
+        });
 
-        return response()->download($newDocumentPath, 'termo.png', $headers);
+        $image->text($dados['curso'], 260, 1245, function ($font) {
+            $font->file(resource_path('fonts/Arial.ttf'));
+            $font->size(42);
+            $font->color('#00009C');
+
+        });
+
+        return $image->response('png');
     }
 
     public function index()
