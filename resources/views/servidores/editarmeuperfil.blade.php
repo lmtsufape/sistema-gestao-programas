d607e3a51d1678e7d82478ed4eedcffbd478b956
MatheusInte <matheus.interaminense.guerra@gmail.com>
2023-08-24 11:23:40 -0300

    Editar Perfil - Servidor

--- resources/views/servidores/editarmeuperfil.blade.php
+++ resources/views/servidores/editarmeuperfil.blade.php
@@ -1,6 +1,6 @@
 @extends("templates.app")
 @section("body")
-        <div class="container-fluid" style="display: flex; justify-content: center; align-items: center; margin-top: 2.5em; margin-bottom:3.6em; ">
+        <div class="container-fluid" style="display: flex; justify-content: center; align-items: center; margin-top: 2.5em; margin-bottom:10em; flex-direction:cloumn;">
 
             @if (session('sucesso'))
                 <div class="alert alert-success">
@@ -8,55 +8,55 @@
                 </div>
             @endif
             <br>
-            <br>
 
-            <div style="background: #FFFFFF; box-shadow: 0px 6px 20px rgba(0, 0, 0, 0.25); border-radius: 20px; padding: 34px; width: 65%";>
-                <h1 style="font-weight: 600; font-size: 30px; line-height: 47px; display: flex; align-items: center; color: #131833;">
-                    Editar Servidor</h1>
-                    <hr>
+            <div class="fundocadastrar">
+                <div class="row" style="align-content: left;">
+                    <h1 class="titulogrande">Editar Perfil</h1>
+                </div>
                     <form action="{{ route('meu-perfil-servidor.atualizar', ['id' => $servidor->id]) }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     @method("PUT")
-
+                    
+                    <div style="display: flex;">
                     @if($servidor->user->image)
-                    <img src="/images/fotos-perfil/{{ $servidor->user->image }}" alt="Foto Perfil" style="width: 150px; height: 150px; border-radius: 50%;"/>
+                    <img src="/images/fotos-perfil/{{ $servidor->user->image }}" alt="Foto Perfil" style="width: 8.77rem; border-radius: 50%; margin-top: 2rem; margin-bottom: 1rem;"/>
                     @else
-                    <img src="/images/sem-foto-perfil.png" alt="Foto Perfil" style="width: 150px; height: 150px; border-radius: 50%;"/>
+                    <img src="/images/sem-foto-perfil.png" alt="Foto Perfil" style="width: 8.77rem; border-radius: 50%; margin-top: 2rem; margin-bottom: 1rem;"/>
                     @endif
-                    <input type="file" id="image" name="image" class="form-control-file">
+                        <div style="align-self:flex-end">
+                        <input type="file" id="image" name="image" class="form-control boxinfo">
+                        </div>
+                    </div>
 
-                    <label for="nome" style="display:flex; font-weight: 600; font-size: 20px; line-height: 28px; color: #131833;">Nome<strong style="color: #8B5558">*</strong></label>
-                    <input type="text" id="nome" name="nome" placeholder="Digite o nome" value="{{$servidor->user->name}}"
-                    style="background: #F5F5F5; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px;
-                    box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);"><br/><br>
+                    <label class="titulopequeno" for="nome_social" for="nome">Nome Social</label>
+                    <input class="boxcadastrar" type="text" id="nome_social" name="nome_social" value="{{$servidor->user->name_social}}">
+                    <br>
 
-                    <label for="nome_social" for="nome" style="display:flex; font-weight: 600; font-size: 20px; line-height: 28px; color: #131833;">Nome Social:</label>
-                    <input type="text" id="nome_social" name="nome_social" value="{{$servidor->user->name_social}}"
-                    style="background: #F5F5F5; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px;
-                    box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);"><br/><br>
+                    <label for="nome" class="titulopequeno">Nome Completo<strong style="color: #8B5558">*</strong></label>
+                    <br>
+                    <input class="boxcadastrar" type="text" id="inputName" name="nome" required placeholder="Digite o nome" value="{{$servidor->user->name}}">
+                    <div class="invalid-feedback"> Por favor preencha esse campo</div>
+                    <br>
 
-                    <label for="email" for="nome" style="display:flex; font-weight: 600; font-size: 20px; line-height: 28px; color: #131833;">E-mail<strong style="color: #8B5558">*</strong></label>
-                    <input type="text" id="email" name="email" placeholder="Digite o e-mail" value="{{$servidor->user->email}}" style="background: #F5F5F5; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px;
-                    box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);"><br/><br>
+                    <label class="titulopequeno" for="email" for="nome">E-mail<strong style="color: #8B5558">*</strong></label>
+                    <input class="boxcadastrar" type="text" id="email" name="email" required placeholder="Digite o e-mail" value="{{$servidor->user->email}}">
+                    <div class="invalid-feedback"> Por favor preencha esse campo</div>
+                    <br>
 
-                    <label for="senha" for="nome" style="display:flex; font-weight: 600; font-size: 20px; line-height: 28px; color: #131833;">Senha<strong style="color: #8B5558">*</strong></label>
-                    <input type="password" id="senha" name="senha" placeholder="Digite a senha" style="background: #F5F5F5; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px;
-                    box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);"><br/><br>
+                    <label class="titulopequeno" for="senha" for="nome">Senha<strong style="color: #8B5558">*</strong></label>
+                    <input class="boxcadastrar" type="password" id="senha" name="senha" required placeholder="Digite a senha">
+                    <div class="invalid-feedback"> Por favor preencha esse campo</div>
+                    <br>
 
-                    <label for="cpf" for="nome" style="display:flex; font-weight: 600; font-size: 20px; line-height: 28px; color: #131833;">CPF<strong style="color: #8B5558">*</strong></label>
-                    <input  class="boxinfo cpf-autocomplete" name="cpf" id="cpf" type="text" placeholder="Digite o CPF" value="{{$servidor->cpf}}" style="background: #F5F5F5; border-radius: 13px; border: 1px #D3D3D3; width: 100%; padding: 5px;
-                    box-shadow: inset 0px 3px 6px rgba(0, 0, 0, 0.25);"><br/><br>
+                    <label class="titulopequeno" for="cpf" for="nome" >CPF<strong style="color: #8B5558">*</strong></label>
+                    <input  class="boxcadastrar boxinfo cpf-autocomplete" name="cpf" id="cpf" type="text" required placeholder="Digite o CPF" value="{{$servidor->cpf}}">
+                    <div class="invalid-feedback"> Por favor preencha esse campo</div>
+                    <br>
+                    <br>
 
-
-                    <div style="display: flex; align-content: center; align-items: center; justify-content: center; gap:5%; padding-top: 15px;">
-                        <input type="button" value="Voltar" href="{{url("/meu-perfil-servidor/")}}" onclick="window.location.href='{{url("/meu-perfil-servidor/")}}'"
-                        style="background: #2D3875; box-shadow: 4px 5px 7px rgba(0, 0, 0, 0.25); display: inline-block;
-                        border-radius: 13px; color: #FFFFFF; border: #2D3875; font-style: normal; font-weight: 400; font-size: 24px;
-                        line-height: 29px; text-align: center; padding: 5px 15px;">
-
-                        <input type="submit" value="Editar" style="background: #34A853; box-shadow: 4px 5px 7px rgba(0, 0, 0, 0.25);
-                        display: inline-block; border-radius: 13px; color: #FFFFFF; border: #34A853; font-style: normal;
-                        font-weight: 400; font-size: 24px; line-height: 29px; text-align: center; padding: 5px 15px;">
+                    <div class="botoessalvarvoltar" style="justify-content: start">
+                        <input class="botaovoltar" type="button" value="Voltar" href="{{url("/meu-perfil-servidor/")}}" onclick="window.location.href='{{url("/meu-perfil-servidor/")}}'">
+                        <input class="botaosalvar" type="submit" value="Editar">
                     </div>
                 </form>
             </div>
