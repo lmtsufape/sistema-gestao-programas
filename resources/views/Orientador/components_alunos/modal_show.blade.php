de72a27c69c840eb05930700734ea6f1437497f9
Anna Victoria Feliciana <anna.feliciana@upe.br>
2023-08-24 18:16:41 -0300

    modais orientador

--- resources/views/Orientador/components_alunos/modal_show.blade.php
+++ resources/views/Orientador/components_alunos/modal_show.blade.php
@@ -1,71 +1,82 @@
-<div class="modal " id="modal_show_{{$pivo->aluno->id}}" tabindex="-1" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered"> <!-- div antes do real modal -->
-    <div class="modal-content modal-create p-3" style="border-radius: 15px; background-color: #F9F9F9; font-family: 'Roboto', sans-serif;">
-      <div class="modal-header">
-        <h5 style="color: #2D3875; font-style: normal; font-weight: 600; font-size: 30px; line-height: 47px; text-align: center;" class="modal-title title fw-bold">Informações do Estudantes</h5>
-        <button class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body" style="text-align: start">
-        <div class="mb-3">
-          <label class="tituloinfomodal form-label mt-3">Nome do aluno:</label>
-          <div class="textoinfomodal"> {{  $pivo->aluno->nome_aluno  }} </div>
+<div class="modal " id="modal_show_{{ $pivo->aluno->id }}" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content modal-create p-2">
+            <div class="modal-header border-0">
+                <p class="titulomodal">Informações do Estudantes</p>
+                    <button class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body" style="text-align: start">
+                <div class="mb-3">
+                    <label class="tituloinfomodal form-label mt-3">Nome do aluno</label>
+                    <div class="textoinfomodal"> {{ $pivo->aluno->nome_aluno }} </div>
+                </div>
+                <div class="mb-3">
+                    <label class="tituloinfomodal form-label mt-3">Edital</label>
+                    <div class="textoinfomodal"> {{ $pivo->edital->titulo_edital }}</div>
+                </div>
+                <div class="mb-3">
 
-          <label class="tituloinfomodal form-label mt-3">Edital:</label>
-          <div class="textoinfomodal"> {{  $pivo->edital->titulo_edital }}</div>
+                    <label class="tituloinfomodal form-label mt-3">Início do edital</label>
+                    <div class="textoinfomodal"> {{ date_format(date_create($pivo->data_inicio), 'd/m/Y') }} </div>
+                </div>
+                <div class="mb-3">
 
-          <label class="tituloinfomodal form-label mt-3">Início do edital:</label>
-          <div class="textoinfomodal"> {{ date_format(date_create($pivo->data_inicio), "d/m/Y") }} </div>
+                    <label class="tituloinfomodal form-label mt-3">Fim do edital</label>
+                    <div class="textoinfomodal"> {{ date_format(date_create($pivo->data_fim), 'd/m/Y') }}</div>
+                </div>
+                <div class="mb-3">
 
-          <label class="tituloinfomodal form-label mt-3">Fim do edital:</label>
-          <div class="textoinfomodal"> {{  date_format(date_create($pivo->data_fim), "d/m/Y") }}</div>
+                    <label class="tituloinfomodal form-label mt-3">Bolsa</label>
+                    <div class="textoinfomodal">{{ $pivo->bolsa }}</div>
+                </div>
+                <div class="mb-3">
 
-          <label class="tituloinfomodal form-label mt-3">Bolsa:</label>
-          <div class="textoinfomodal">{{  $pivo->bolsa }}</div>
+                    <label class="tituloinfomodal form-label mt-3">Informações complementares</label>
+                    <div class="textoinfomodal"> {{ $pivo->info_complementares }}</div>
+                </div>
 
-          <label class="tituloinfomodal form-label mt-3">Informações complementares:</label>
-          <div class="textoinfomodal"> {{  $pivo->info_complementares }}</div>
 
+            </div>
+            <div class="modal-footer border-0">
+                <button type="button" class="btn" data-bs-dismiss="modal">Fechar</button>
+            </div>
         </div>
-        <div class="modal-footer border-0">
-          <button type="button"  class="btn" data-bs-dismiss="modal">Fechar</button>
-        </div>
-      </div>
     </div>
-  </div>
+</div>
 </div>
 <style>
-  .btn {
-    color: #fff;
-    background: #34A853;
-    border-color: #34A853;
-    border-radius: 20px;
-    width:120px;
-  }
+    .btn {
+        color: #fff;
+        background: #34A853;
+        border-color: #34A853;
+        border-radius: 20px;
+        width: 120px;
+    }
 
-  .btn:hover {
-    background-color: #40b760;
-    border-color: #40b760;
-    color: #fff;
-  }
+    .btn:hover {
+        background-color: #40b760;
+        border-color: #40b760;
+        color: #fff;
+    }
 
-  .link{
-    background: #EEEEEE;
-    border-radius: 13px;
-    border: 1px #D3D3D3;
-    width: 100%;
-    display:flex;
-    flex-direction:row;
-    flex-wrap:wrap;
-    justify-content:center;
-    align-items:center;
-    padding:5px;
-    margin:2px;
-    text-decoration: none;
-    color: #2D3875;
-  }
+    .link {
+        background: #EEEEEE;
+        border-radius: 13px;
+        border: 1px #D3D3D3;
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+        padding: 5px;
+        margin: 2px;
+        text-decoration: none;
+        color: #2D3875;
+    }
 
-  .link:hover{
-    text-decoration: none;
-    color:#34A853;
-  }
+    .link:hover {
+        text-decoration: none;
+        color: #34A853;
+    }
 </style>
--- resources/views/Orientador/components_programa/modal_show.blade.php
+++ resources/views/Orientador/components_programa/modal_show.blade.php
@@ -1,36 +1,32 @@
 <div class="modal" id="modal_show_" tabindex="-1" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content modal-create p-3" style="border-radius: 15px; background-color: #F9F9F9; font-family: 'Roboto', sans-serif;">
-      <div class="modal-header">
-        <h5 style="color: #2D3875; font-style: normal; font-weight: 600; font-size: 30px; line-height: 47px;" class="modal-title title fw-bold">Informações do Programa</h5>
-        <button class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body" style="text-align: start">
-        <div class="row mb-3">
-          <label for="nome_edit" style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833; margin-bottom:8px;" class=" form-label mt-3">
-            Nome:</label>
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content modal-create p-2">
+            <div class="modal-header border-0">
+                <p class="titulomodal">Informações do Programa</p>
+                <button class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body" style="text-align: start">
+                <div class="mb-3">
+                    <label class="tituloinfomodal form-label mt-3">Nome</label>
 
-          <div class="textoinfomodal">
+                    <div class="textoinfomodal">
 
-          </div>
+                    </div>
 
-          <label for="nome_edit" style="display:flex; font-weight: 400; font-size: 20px; line-height: 28px; color: #131833; margin-bottom:8px;" class=" form-label mt-3">
-            Descrição:</label>
-
-          <div class="textoinfomodal">
-
-          </div>
-          <!-- @if(!empty($errors->update->first('name')))
-          <span class="invalid-feedback d-block">
-            <strong> {{$errors->update->first('name')}} </strong>
+                    <label class="tituloinfomodal form-label mt-3">Descrição</label>
+                    <div class="textoinfomodal">
+                    </div>
+                    <!-- @if (!empty($errors->update->first('name')))
+<span class="invalid-feedback d-block">
+            <strong> {{ $errors->update->first('name') }} </strong>
           </span>
-          @endif -->
+@endif -->
+                </div>
+            </div>
+            <div class="modal-footer border-0">
+                <button type="button" style="background: #34A853; border: #34A853;" class="btn btn-secondary"
+                    data-bs-dismiss="modal">Fechar</button>
+            </div>
         </div>
-      </div>
-      <div class="modal-footer border-0">
-        <button type="button" style="background: #34A853; border: #34A853;" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-      </div>
     </div>
-  </div>
 </div>
-
