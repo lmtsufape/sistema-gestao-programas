19dcdff923e60d378ced93ec43179467cdc4f0b0
MatheusInte <matheus.interaminense.guerra@gmail.com>
2023-08-28 15:08:19 -0300

    Redesign edição de informações de instituição

--- resources/views/Estagio/instituicao/editInstituicao.blade.php
+++ resources/views/Estagio/instituicao/editInstituicao.blade.php
@@ -16,79 +16,70 @@
             <h1 class="titulogrande">Editar Instituição</h1>
         </div>
 
-        <hr style="color: #5C1C26; background-color: #5C1C26">
+        <br>
 
         <form action="{{ route('instituicao.update') }}" method="POST">
             @csrf
             @method("POST")
 
-            <div class="form-group">
-                <label for="nome">Instituição:</label>
-                <input type="text" name="instituicao" id="instituicao" class="form-control" value="{{ $instituicao->instituicao }}" required>
+                <label class="titulopequeno" for="nome">Instituição:</label>
+                <input class="boxcadastrar" type="text" name="instituicao" id="instituicao" class="form-control" value="{{ $instituicao->instituicao }}" required>
+
+            
+                <label class="titulopequeno" for="sigla">Sigla:</label>
+                <input class="boxcadastrar" type="text" name="sigla" id="sigla" class="form-control" value="{{ $instituicao->sigla }}" required>
+
+            
+                <label class="titulopequeno" for="sigla">CNPJ:</label>
+                <input class="boxcadastrar" type="text" name="cnpj" id="cnpj" class="form-control" value="{{ $instituicao->cnpj }}" required>
+
+            
+                <label class="titulopequeno" for="sigla">Natureza Jurídica da Instituição:</label>
+                <input class="boxcadastrar" type="text" name="natureza_juridica" id="natureza_juridica" class="form-control" value="{{ $instituicao->natureza_juridica }}" required>
+
+            
+                <label class="titulopequeno" for="sigla">Endereço:</label>
+                <input class="boxcadastrar" type="text" name="endereco" id="endereco" class="form-control" value="{{ $instituicao->endereco }}" required>
+
+            
+                <label class="titulopequeno" for="sigla">Número:</label>
+                <input class="boxcadastrar" type="text" name="numero" id="numero" class="form-control" value="{{ $instituicao->numero }}" required>
+
+            
+                <label class="titulopequeno" for="sigla">Complemento:</label>
+                <input class="boxcadastrar" type="text" name="complemento" id="complemento" class="form-control" value="{{ $instituicao->complemento }}" required>
+
+            
+                <label class="titulopequeno" for="sigla">CEP:</label>
+                <input class="boxcadastrar" type="text" name="CEP" id="CEP" class="form-control" value="{{ $instituicao->CEP }}" required>
+
+            
+                <label class="titulopequeno" for="sigla">Bairro:</label>
+                <input class="boxcadastrar" type="text" name="bairro" id="bairro" class="form-control" value="{{ $instituicao->bairro }}" required>
+
+            
+                <label class="titulopequeno" for="sigla">Cidade:</label>
+                <input class="boxcadastrar" type="text" name="cidade" id="cidade" class="form-control" value="{{ $instituicao->cidade }}" required>
+
+            
+                <label class="titulopequeno" for="sigla">Estado:</label>
+                <input class="boxcadastrar" type="text" name="estado" id="estado" class="form-control" value="{{ $instituicao->estado }}" required>
+
+            
+                <label class="titulopequeno" for="sigla">Representante:</label>
+                <input class="boxcadastrar" type="text" name="representante" id="representante" class="form-control" value="{{ $instituicao->representante }}" required>
+
+            
+                <label class="titulopequeno" for="sigla">Cargo do Representante:</label>
+                <input class="boxcadastrar" type="text" name="cargo_representante" id="cargo_representante" class="form-control" value="{{ $instituicao->cargo_representante }}" required>
+
+                <br>
+                <br>
+
+            <div class="botoessalvarvoltar">
+                        <input type="button" value="Voltar" href="{{ route('instituicao.index')}}" onclick="window.location.href='{{ route("instituicao.index")}}'" class="botaovoltar">
+                        <input class="botaosalvar" type="submit" value="Salvar">
             </div>
-
-            <div class="form-group">
-                <label for="sigla">Sigla:</label>
-                <input type="text" name="sigla" id="sigla" class="form-control" value="{{ $instituicao->sigla }}" required>
-            </div>
-
-            <div class="form-group">
-                <label for="sigla">CNPJ:</label>
-                <input type="text" name="cnpj" id="cnpj" class="form-control" value="{{ $instituicao->cnpj }}" required>
-            </div>
-
-            <div class="form-group">
-                <label for="sigla">Natureza Jurídica da Instituição:</label>
-                <input type="text" name="natureza_juridica" id="natureza_juridica" class="form-control" value="{{ $instituicao->natureza_juridica }}" required>
-            </div>
-
-            <div class="form-group">
-                <label for="sigla">Endereço:</label>
-                <input type="text" name="endereco" id="endereco" class="form-control" value="{{ $instituicao->endereco }}" required>
-            </div>
-
-            <div class="form-group">
-                <label for="sigla">Número:</label>
-                <input type="text" name="numero" id="numero" class="form-control" value="{{ $instituicao->numero }}" required>
-            </div>
-
-            <div class="form-group">
-                <label for="sigla">Complemento:</label>
-                <input type="text" name="complemento" id="complemento" class="form-control" value="{{ $instituicao->complemento }}" required>
-            </div>
-
-            <div class="form-group">
-                <label for="sigla">CEP:</label>
-                <input type="text" name="CEP" id="CEP" class="form-control" value="{{ $instituicao->CEP }}" required>
-            </div>
-
-            <div class="form-group">
-                <label for="sigla">Bairro:</label>
-                <input type="text" name="bairro" id="bairro" class="form-control" value="{{ $instituicao->bairro }}" required>
-            </div>
-
-            <div class="form-group">
-                <label for="sigla">Cidade:</label>
-                <input type="text" name="cidade" id="cidade" class="form-control" value="{{ $instituicao->cidade }}" required>
-            </div>
-
-            <div class="form-group">
-                <label for="sigla">Estado:</label>
-                <input type="text" name="estado" id="estado" class="form-control" value="{{ $instituicao->estado }}" required>
-            </div>
-
-            <div class="form-group">
-                <label for="sigla">Representante:</label>
-                <input type="text" name="representante" id="representante" class="form-control" value="{{ $instituicao->representante }}" required>
-            </div>
-
-            <div class="form-group">
-                <label for="sigla">Cargo do Representante:</label>
-                <input type="text" name="cargo_representante" id="cargo_representante" class="form-control" value="{{ $instituicao->cargo_representante }}" required>
-            </div>
-
-
-            <button type="submit" class="btn btn-primary">Salvar Alterações</button>
         </form>
     </div>
 </div>
