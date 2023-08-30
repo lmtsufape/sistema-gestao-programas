0582089398c7469fb375498d3d9be20b399f73a1
jonas <jonas.henrique@upe.br>
2023-08-30 15:39:03 -0300

    correçoes

--- resources/views/Estagio/documentos/plano_de_atividades.blade.php
+++ resources/views/Estagio/documentos/plano_de_atividades.blade.php
@@ -1,8 +1,40 @@
 @extends('templates.app')
 
 @section('body')
+    <div class="fundocadastrar">
+        <div class="row" style="align-content: left;">
+            <h1 class="titulogrande">Termo de encaminhamento</h1>
+        </div>
+        @if (Session::has('pdf_generated_success'))
+            <div class="alert alert-success">
+                {{ Session::get('pdf_generated_success') }}
+            </div>
+        @endif
+
+        <hr style="color:#5C1C26; background-color: #5C1C26">
+
+        <form action="{{ route('estagio.documentos.termo-de-encaminhamento.store', ['id' => $estagio->id]) }}"method="post">
+            @csrf
 
 
 
+            <label for="nome" class="titulopequeno">Periodo<strong style="color: #8B5558">*</strong></label>
+            <br>
+            <input class="boxcadastrar" type="text" name="periodo" id="periodo" placeholder="Digite o periodo do curso"
+                required><br><br>
+            <div class="invalid-feedback"> Por favor preencha esse campo</div><br><br>
 
+
+            <br><br>
+            <div class="botoessalvarvoltar">
+                <input type="button" value="Voltar" href="{{ url('/home/') }}"
+                    onclick="window.location.href='{{ url('/home/') }}'" class="botaovoltar">
+                <input class="botaosalvar" type="submit" value="Salvar">
+            </div>
+
+
+        </form>
+
+    </div>
+    </div>
 @endsection
