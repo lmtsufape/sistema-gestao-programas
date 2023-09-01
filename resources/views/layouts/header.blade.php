f2c6b04dc65c4c3f27c867aecf7adbc8c53361af
Camila Claudino <camila.claudinoo123@gmail.com>
2023-09-01 14:55:02 -0300

    ajustando o headder

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,6 @@
 @section('css')
-    <link rel="stylesheet" href="/css/app.css">
-    <link rel="stylesheet" href="/css/header.css">
+<link rel="stylesheet" href="/css/app.css">
+<link rel="stylesheet" href="/css/header.css">
 @endsection
 
 <head>
@@ -8,23 +8,22 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  </head>
+</head>
 
 <header>
     <!-- Isso aqui é a barra de cima -->
-    <div>
-      <nav class="navbar navbar-expand-lg fundoheader">
-          <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg fundoheader">
+        <div class="container-fluid">
             <ul class="nav navbar-nav me-auto mb-2 mb-lg-0">
-              @auth
-                <a href="{{route('home')}}" type="button" style="margin-left: 5em" >
-                  <img src="{{asset("images/Logo-SGPA.svg")}}" alt="Logo da SGPA" style="height: auto; width: auto">
+                @auth
+                <a href="{{route('home')}}" type="button" style="margin-left: 5em">
+                    <img src="{{asset("images/Logo-SGPA.svg")}}" alt="Logo da SGPA" style="margin-top: 5px; height: 75px; width: 245px">
                 </a>
-              @else
-                  <a href="{{url('/')}}" type="button">
-                      <img src="{{asset("images/Logo-SGPA.svg")}}" title="Logo SGPA" alt="Logo da SGPA">
-                  </a>
-              @endauth
+                @else
+                <a href="{{url('/')}}" type="button">
+                    <img src="{{asset("images/Logo-SGPA.svg")}}" title="Logo SGPA" style="margin-top: 5px; height: 75px; width: 245px" alt="Logo da SGPA">
+                </a>
+                @endauth
             </ul>
 
             <div>
@@ -40,19 +39,19 @@
 
                     <h2 class="fonteheader">
                         @auth
-                            Olá, {{Auth::user()->name}}
+                        Olá, {{Auth::user()->name}}
                         @endauth
                     </h2>
 
                     @auth
-                    <div class="dropdown"  style="margin-top: 10px; margin-right: 5em">
+                    <div class="dropdown" style="margin-top: 10px; margin-right: 5em">
                         <button class="botaoinvisivel" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <img src="{{ asset('images/arrowdown.svg') }}" title="Opções do meu perfil" alt="Seta para baixo" style="height: auto; width: auto">
                         </button>
 
                         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton" style="padding: 15px">
                             @if (auth()->user()->typage_type == "App\Models\Servidor")
-                            {{--  a com icon de UserVector  --}}
+                            {{-- a com icon de UserVector  --}}
                             <a href="{{ route('meu-perfil-servidor') }}" class="caixinhasetinha">
                                 <img src="{{ asset('images/UserVector.svg') }}" alt="Icone de usuário">
                                 <span style="margin-left: 5px;">Meu perfil</span>
@@ -89,18 +88,16 @@
                     @auth
 
                 </div>
-              @else
-              <div style="text-align: right;">
-                <a href="/" class="fonteheader">Início</a>
-                <a href="/sistema" class="fonteheader">O Sistema</a>
-                <a href="/parceria" class="fonteheader">A Parceria</a>
-                <a href="/contato" class="fonteheader">Contato</a>
-              </ul>
+                @else
+                <div style="text-align: right;">
+                    <a href="/" class="fonteheader">Início</a>
+                    <a href="/sistema" class="fonteheader">O Sistema</a>
+                    <a href="/parceria" class="fonteheader">A Parceria</a>
+                    <a href="/contato" class="fonteheader">Contato</a>
+                    </ul>
 
-              @endauth
+                    @endauth
+                </div>
             </div>
-          </div>
-        </nav>
-    </div>
-  </header>
-
+    </nav>
+</header>
