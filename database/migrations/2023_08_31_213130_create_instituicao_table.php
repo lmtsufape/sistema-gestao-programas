bd3d681d0f3d74cf237876c3be5774b8ff848a27
Guilherme Rivaldy <Guilhermerivaldy288@hotmail.com>
2023-08-31 21:38:46 -0300

    Fazendo nova migration

---
+++ database/migrations/2023_08_31_213130_create_instituicao_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInstituicaoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('instituicao', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('instituicao');
+            $table->string('sigla');
+            $table->string('cnpj',14)->unique();
+            $table->string('natureza_juridica');
+            $table->string('endereco');
+            $table->string('numero');
+            $table->string('complemento');
+            $table->string('CEP',8)->unique();
+            $table->string('bairro');
+            $table->string('cidade');
+            $table->string('estado');
+            $table->string('representante');
+            $table->string('cargo_representante');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('instituicao');
+    }
+}
---
+++ database/migrations/2023_08_31_213718_create_estagios_table.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEstagiosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estagios', function (Blueprint $table) {
+            $table->softDeletes();
+            $table->id();
+            $table->timestamps();
+            $table->boolean('status')->default(True);
+            $table->text('descricao');
+            //$table->string('cpf_aluno');
+            $table->date('data_inicio');
+            $table->date('data_fim');
+            //$table->date('data_solicitacao');
+            $table->enum('tipo', ['eo', 'eno']);
+
+            //$table->foreign('cpf_aluno')->references('cpf')->on('alunos');
+            //$table->unsignedBigInteger('cpf');
+
+            $table->unsignedBigInteger('aluno_id');
+            $table->foreign('aluno_id')->references('id')->on('alunos');
+
+            $table->unsignedBigInteger('orientador_id');
+            $table->foreign('orientador_id')->references('id')->on('orientadors');
+            
+            $table->unsignedBigInteger('curso_id');
+            $table->foreign('curso_id')->references('id')->on('cursos');
+
+            $table->unsignedBigInteger('disciplina_id')->nullable(true);
+            $table->foreign('disciplina_id')->references('id')->on('disciplinas');
+
+            $table->unsignedBigInteger('supervisor_id');
+            $table->foreign('supervisor_id')->references('id')->on('supervisors');
+
+            $table->unsignedBigInteger('instituicao_id')->default(1);
+            $table->foreign('instituicao_id')->references('id')->on('instituicao');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estagios');
+    }
+}
