a8a4bae267f849625168453861360c007cdd630b
Camila Claudino <camila.claudinoo123@gmail.com>
2023-09-05 16:23:11 -0300

    deixando o footer mais responsivo

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,20 +4,20 @@
 @endsection
 
 <footer class="footer">
-    <div class="container-fluid">
-        <div class="row">
-            <!-- Logos à esquerda -->
-            <div class="col-3" style="margin-top: 20px; padding-left: 100px">
-                <div class="d-flex align-items-center justify-content-start">
-                    <a href="" title="Logo do SGPA" target="_blank">
-                        <img src="{{asset('images/sgpa-branco 1.svg')}}" alt="Logo SGPA" style="margin-left: 20px; height: 60px;">
-                    </a>
+    <div  style="width: 100%" >
+        <div class="container-fluid">
+            <div class="row d-flex  justify-content-between" style="flex-wrap: wrap;">
+                <!-- Logos à esquerda -->
+                <div style="margin-top: 20px; padding-left: 10px; width: fit-content; ">
+                    <div class="d-flex align-items-center justify-content-start">
+                        <a href="" title="Logo do SGPA" target="_blank">
+                            <img src="{{asset('images/sgpa-branco 1.svg')}}" alt="Logo SGPA" style="height: 60px;">
+                        </a>
+                    </div>
                 </div>
-            </div>
 
-            <!-- Logos do centro -->
-            <div class="col-6 d-flex justify-content-center" style="margin-top: 14px">
-                <div class="d-flex justify-content-around" style="align-self: center; gap: 5%">
+                <!-- Logos do centro -->
+                <div class="d-flex justify-content-between" style=" gap: 5%; margin-top: 14px; width: fit-content; ">
                     <a href="http://ufape.edu.br/" title="Ir para o site da UFAPE" target="_blank">
                         <img src="{{ asset('images/logo_ufape_vertical.png') }}" alt="Logo UFAPE" class="logo-box" style="height: 49.813px; width: 200.545px,flex-shrink 0; border-radius: 10px;">
                     </a>
@@ -28,11 +28,10 @@
                         <img src="{{ asset('images/logoupe.png') }}" alt="Logo UPE" class="logo-box" style="height: 49.813px; width: 200.545px,flex-shrink 0 ; border-radius: 10px">
                     </a>
                 </div>
-            </div>
 
-            <!-- Logos à direita -->
-            <div class="col-3 d-flex justify-content-center">
-                <div class="d-flex justify-content-around" style="align-self: center; gap: 5%">
+
+                <!-- Logos à direita -->
+                <div class="d-flex justify-content-between" style="align-self: center; gap: 5%; width: fit-content;">
                     <a href="https://www.facebook.com/LMTSUFAPE/" title="Facebook do LMTS" target="_blank">
                         <img src="{{asset('images/logo_facebook_branco.svg')}}" alt="Logo Facebook" style="height: 40px; padding-right:20px">
                     </a>
@@ -45,6 +44,5 @@
                 </div>
             </div>
         </div>
-    </div>
     </div>
 </footer>
