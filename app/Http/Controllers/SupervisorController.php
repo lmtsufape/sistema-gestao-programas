668541c298a907088b0373d3e179a749ea59c519
Itamar Bernardo <itamarbernardo2013@gmail.com>
2023-09-13 12:55:32 -0300

    Implementado fluxo para cadastrar um supervisor

--- app/Http/Controllers/SupervisorController.php
+++ app/Http/Controllers/SupervisorController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Supervisor;
+use Illuminate\Support\Facades\DB;
 
 class SupervisorController extends Controller
 {
@@ -18,4 +19,32 @@ class SupervisorController extends Controller
     
     }
 
+    public function create(){
+        return view("Supervisor.cadastrar");
+    }
+
+
+    public function store(Request $request){
+        DB::beginTransaction();
+        try {
+            $supervisor = new Supervisor();
+            $supervisor->nome = $request->nome;
+            $supervisor->cpf = $request->cpf;
+            $supervisor->email = $request->email;
+            $supervisor->telefone = $request->telefone;
+            $supervisor->formacao = $request->formacao;
+            
+            if ($supervisor->save()) {
+                
+                DB::commit();
+
+                return redirect('/supervisor')->with('sucesso', 'Supervisor cadastrado com sucesso.');
+            } else {
+                return redirect()->back()->withErrors("Falha ao cadastrar Supervisor. Tente novamente mais tarde.");
+            }
+        } catch (Exception $e) {
+            DB::rollback();
+            return redirect()->back()->withErrors("Falha ao cadastrar Supervisor. Tente novamente mais tarde.");
+        }
+    }
 }
