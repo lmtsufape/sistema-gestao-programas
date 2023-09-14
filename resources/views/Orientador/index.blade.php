29f903c80ceb25d155d1e764b1460e2f7db4bbeb
Lucas Melo <lmelo072002@gmail.com>
2023-09-13 23:03:44 -0300

    Issue #840 Mudancas perfil reitor parte 2 - primeira versao

--- resources/views/Orientador/index.blade.php
+++ resources/views/Orientador/index.blade.php
@@ -43,11 +43,13 @@
 
             {{-- TODO: Falta adicionar um modal com os possiveis filtros  --}}
             <form class="search-container" action="{{ route('orientadors.index') }}" method="GET">
-                <input class="search-input" onkeyup="" type="text" placeholder="Digite a busca" title="" id="valor"
-                    name="valor" style="text-align: start">
+                <input class="search-input" onkeyup="" type="text" placeholder="Digite a busca" title=""
+                    id="valor" name="valor" style="text-align: start">
                 <input class="search-button" title="Fazer a pesquisa" type="submit" value=""></input>
-                <button class="cadastrar-botao" type="button"
-                    onclick="window.location.href = '{{ route('orientadors.create') }}'">Cadastrar professor</button>
+                @if (Auth::user()->typage->tipo_servidor != 'pro_reitor')
+                    <button class="cadastrar-botao" type="button"
+                        onclick="window.location.href = '{{ route('orientadors.create') }}'">Cadastrar professor</button>
+                @endif
             </form>
 
         </div>
@@ -91,23 +93,25 @@
                                     <td class="align-middle">
                                         <a type="button" data-bs-toggle="modal"
                                             data-bs-target="#modal_show_{{ $orientador->id }}">
-                                            <img src="{{ asset('images/information.svg') }}" title="Informações" alt="Info professor"
-                                                style="height: 30px; width: 30px;">
+                                            <img src="{{ asset('images/information.svg') }}" title="Informações"
+                                                alt="Info professor" style="height: 30px; width: 30px;">
                                         </a>
                                         {{--  <a type="button" data-bs-toggle="modal" data-bs-target="#modal_documents_{{$orientador->id}}">
                                 <img src="{{asset('images/document.png')}}" alt="Documento professor"  style="height: 30px; width: 30px;">
 
                                 </a> --}}
-                                        <a href=" {{ route('orientadors.edit', ['id' => $orientador->id]) }}">
-                                            <img src="{{ asset('images/pencil.svg') }}" title="Editar" alt="Editar professor"
-                                                style="height: 30px; width: 30px;">
-                                        </a>
-                                        <a type="button" data-bs-toggle="modal"
-                                            data-bs-target="#modal_delete_{{ $orientador->id }}">
-                                            <img src="{{ asset('images/delete.svg') }}" title="Remover" alt="Deletar professor"
-                                                style="height: 30px; width: 30px;">
-                                        </a>
 
+                                        @if (Auth::user()->typage->tipo_servidor != 'pro_reitor')
+                                            <a href=" {{ route('orientadors.edit', ['id' => $orientador->id]) }}">
+                                                <img src="{{ asset('images/pencil.svg') }}" title="Editar"
+                                                    alt="Editar professor" style="height: 30px; width: 30px;">
+                                            </a>
+                                            <a type="button" data-bs-toggle="modal"
+                                                data-bs-target="#modal_delete_{{ $orientador->id }}">
+                                                <img src="{{ asset('images/delete.svg') }}" title="Remover"
+                                                    alt="Deletar professor" style="height: 30px; width: 30px;">
+                                            </a>
+                                        @endif
 
                                     </td>
                                 </tr>
@@ -124,40 +128,40 @@
                     </table>
                 </div>
                 <!--
-              <div style="background-color: #F2F2F2; border-radius: 10px; margin-top: 7px; box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.25);
+                              <div style="background-color: #F2F2F2; border-radius: 10px; margin-top: 7px; box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.25);
         width: 150px; height: 50%;">
-                <div style="align-self: center; margin-right: auto">
-                  <br>
-                  <h4 class="fw-bold" style="font-size: 15px; color:#2D3875;">Legenda dos ícones:</h4>
-                </div>
+                                <div style="align-self: center; margin-right: auto">
+                                  <br>
+                                  <h4 class="fw-bold" style="font-size: 15px; color:#2D3875;">Legenda dos ícones:</h4>
+                                </div>
 
-                <div style="align-self: center; margin-right: auto">
-                  <div style="display: flex; margin: 10px">
-                    <a><img src="/images/info.png" alt="Informacoes" style="width: 20px; height: 20px;"></a>
-                    <p style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%; margin:5px">Informações</p>
-                  </div>
-                  {{--  <div style="display: flex; margin: 10px">
+                                <div style="align-self: center; margin-right: auto">
+                                  <div style="display: flex; margin: 10px">
+                                    <a><img src="/images/info.png" alt="Informacoes" style="width: 20px; height: 20px;"></a>
+                                    <p style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%; margin:5px">Informações</p>
+                                  </div>
+                                  {{--  <div style="display: flex; margin: 10px">
             <a><img src="/images/document.png" alt="Documentos" style="width: 20px; height: 20px;"></a>
             <p style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%; margin:5px">Documentos</p>
           </div>  --}}
-                </div>
-                <div style="align-self: center; margin-right: auto">
-                  <div style="display: flex; margin: 10px">
-                    <a><img src="/images/edit-outline-blue.png" alt="Editar" style="width: 20px; height: 20px;"></a>
-                    <p style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%; margin:5px">Editar</p>
-                  </div>
-                  <div style="display: flex; margin: 10px">
-                    <a><img src="{{ asset('images/delete.png') }}" alt="Deletar orientador" style="width: 20px; height: 20px;"></a>
-                    <p style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%; margin:5px">Deletar</p>
-                  </div>
-                  <div style="display: flex; margin: 10px">
-                    <a><img src="{{ asset('images/searchicon.png') }}" alt="Procurar" style="width: 20px; height: 20px;"></a>
-                    <p style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%; margin:5px">Pesquisar</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-                  -->
+                                </div>
+                                <div style="align-self: center; margin-right: auto">
+                                  <div style="display: flex; margin: 10px">
+                                    <a><img src="/images/edit-outline-blue.png" alt="Editar" style="width: 20px; height: 20px;"></a>
+                                    <p style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%; margin:5px">Editar</p>
+                                  </div>
+                                  <div style="display: flex; margin: 10px">
+                                    <a><img src="{{ asset('images/delete.png') }}" alt="Deletar orientador" style="width: 20px; height: 20px;"></a>
+                                    <p style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%; margin:5px">Deletar</p>
+                                  </div>
+                                  <div style="display: flex; margin: 10px">
+                                    <a><img src="{{ asset('images/searchicon.png') }}" alt="Procurar" style="width: 20px; height: 20px;"></a>
+                                    <p style="font-style: normal; font-weight: 400; font-size: 15px; line-height: 130%; margin:5px">Pesquisar</p>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                                  -->
                 {{-- <div style="margin: auto; width: 45%; padding: 10px;">
         <div class="pagination">
           <a href="#" style="border-radius: 15px; background: #131833; color: white;">Anterior</a>
