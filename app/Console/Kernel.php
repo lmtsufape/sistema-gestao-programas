3823d50348139a3c687f9abcebf5a204a9604058
Guilherme Rivaldy <Guilhermerivaldy288@hotmail.com>
2023-12-18 19:17:38 -0300

    feat: inclusão do comando ao kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('Update:StatusEstagio')->daily();
+        $schedule->command('limpar:arquivos')->daily();
     }
 
     /**
