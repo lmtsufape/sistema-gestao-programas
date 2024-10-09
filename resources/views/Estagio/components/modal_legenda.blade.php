c4e962f46edfa71b4ede38189dc19b18b3cc5626
Rener Tomé <55724549+Rener1@users.noreply.github.com>
2024-10-09 17:23:21 -0300

    Correção em alguns modais.

--- resources/views/Estagio/components/modal_legenda.blade.php
+++ resources/views/Estagio/components/modal_legenda.blade.php
@@ -5,47 +5,48 @@
                 <p class="titulomodal">Legenda dos ícones</p>
                 <button class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-
+            
             <div class="modal-body" style="text-align: start">
-                @can('visualizar documento estagio')
-                    <div class="mb-3">
-                        <img src="{{ asset('images/information_red.svg') }}" alt="Info estágio"
-                            style="height: 30px; width: 30px; padding-bottom: 5px">
-                        <span class="textoinfomodal">Visualizar observação do documento</span>
-                    </div>
-                    <div class="mb-3">
-                        <img src="{{ asset('images/file_red.svg') }}" alt="Info estágio"
-                            style="height: 30px; width: 30px; padding-bottom: 5px">
-                        <span class="textoinfomodal">Visualizar documentos</span>
-                    </div>
-                @endcan
-                @can('editar estagio')
-                    <div class="mb-3">
-                        <img src="{{ asset('images/pencil_red.svg') }}" alt="Editar estágio"
-                            style="height: 30px; width: 30px; padding-bottom: 5px">
-                        <span class="textoinfomodal">Editar o estágio</span>
-                    </div>
-                @endcan
-                @can('visualizar documento estagio')
-                    <div class="mb-3">
-                        <img src="{{ asset('images/file_red.svg') }}" alt="Ver documentos do estágio"
-                            style="height: 30px; width: 30px; padding-bottom: 5px">
-                        <span class="textoinfomodal">Ver os documentos do estágio</span>
-                    </div>
-                @endcan
-                @can('deletar estagio')
-                    <div class="mb-3">
-                        <img src="{{ asset('images/delete_red.svg') }}" alt="Deletar estágio"
-                            style="height: 30px; width: 30px; padding-bottom: 5px">
-                        <span class="textoinfomodal">Deletar o estágio</span>
-                    </div>
-                @endcan
                 @role('estudante')
                     <div class="mb-3">
                         <img src="{{ asset('images/mostrar-documentos-red.svg') }}" alt="Ver documentos do estágio"
                             style="height: 30px; width: 30px; padding-bottom: 5px">
                         <span class="textoinfomodal">Ver os documentos do estágio</span>
                     </div>
+                @else
+                    @can('visualizar documento estagio')
+                        <div class="mb-3">
+                            <img src="{{ asset('images/information_red.svg') }}" alt="Info estágio"
+                            style="height: 30px; width: 30px; padding-bottom: 5px">
+                            <span class="textoinfomodal">Visualizar observação do documento</span>
+                        </div>
+                        <div class="mb-3">
+                            <img src="{{ asset('images/file_red.svg') }}" alt="Info estágio"
+                            style="height: 30px; width: 30px; padding-bottom: 5px">
+                            <span class="textoinfomodal">Visualizar documentos</span>
+                        </div>
+                    @endcan
+                    @can('editar estagio')
+                        <div class="mb-3">
+                            <img src="{{ asset('images/pencil_red.svg') }}" alt="Editar estágio"
+                            style="height: 30px; width: 30px; padding-bottom: 5px">
+                            <span class="textoinfomodal">Editar o estágio</span>
+                        </div>
+                    @endcan
+                    @can('visualizar documento estagio')
+                        <div class="mb-3">
+                            <img src="{{ asset('images/file_red.svg') }}" alt="Ver documentos do estágio"
+                            style="height: 30px; width: 30px; padding-bottom: 5px">
+                            <span class="textoinfomodal">Ver os documentos do estágio</span>
+                        </div>
+                    @endcan
+                    @can('deletar estagio')
+                        <div class="mb-3">
+                            <img src="{{ asset('images/delete_red.svg') }}" alt="Deletar estágio"
+                            style="height: 30px; width: 30px; padding-bottom: 5px">
+                            <span class="textoinfomodal">Deletar o estágio</span>
+                        </div>
+                    @endcan
                 @endrole
             </div>
             <div class="modal-footer border-0"></div>
--- resources/views/Estagio/components/modal_show.blade.php
+++ resources/views/Estagio/components/modal_show.blade.php
@@ -1,74 +1,73 @@
-@can('visualizar estagio')
-    <div class="modal " id="modal_show{{ $estagio->id }}" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content modal-create p-2">
+<div class="modal " id="modal_show{{ $estagio->id }}" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content modal-create p-2">
+            @can('visualizar estagio')
                 <div class="modal-header border-0">
                     <p class="titulomodal">Informações do Estágio</p>
-                    <button class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body" style="text-align: start">
-                    <div class="mb-3">
-                        <label class="tituloinfomodal form-label mt-3">Status</label>
-                        <div class="textoinfomodal">
-                            @if ($estagio->status == 0)
-                                {{ 'Inativo' }}
-                            @else
-                                {{ 'Ativo' }}
-                            @endif
+                        <button class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body" style="text-align: start">
+                        <div class="mb-3">
+                            <label class="tituloinfomodal form-label mt-3">Status</label>
+                            <div class="textoinfomodal">
+                                @if ($estagio->status == 0)
+                                    {{ 'Inativo' }}
+                                @else
+                                    {{ 'Ativo' }}
+                                @endif
+                            </div>
                         </div>
-                    </div>
-                    <div class="mb-3">
+                        <div class="mb-3">
 
-                        <label class="tituloinfomodal form-label mt-3">Descrição</label>
-                        <div class="textoinfomodal"> {{ $estagio->descricao }} </div>
-                    </div>
-                    <div class="mb-3">
-
-                        <label class="tituloinfomodal form-label mt-3">Tipo</label>
-                        <div class="textoinfomodal">
-                            @if ($estagio->tipo == 'eno')
-                                {{ 'Não-Obrigatório' }}
-                            @else
-                                {{ 'Obrigatório' }}
-                            @endif
+                            <label class="tituloinfomodal form-label mt-3">Descrição</label>
+                            <div class="textoinfomodal"> {{ $estagio->descricao }} </div>
                         </div>
-                    </div>
-                    <div class="mb-3">
-                        <label class="tituloinfomodal form-label mt-3">Data de início</label>
-                        <div class="textoinfomodal"> {{ date_format(date_create($estagio->data_inicio), 'd/m/Y') }}</div>
-                    </div>
-                    <div class="mb-3">
-                        <label class="tituloinfomodal form-label mt-3">Data de fim</label>
-                        <div class="textoinfomodal"> {{ date_format(date_create($estagio->data_fim), 'd/m/Y') }}</div>
-                    </div>
-                    <div class="mb-3">
-                        <label class="tituloinfomodal form-label mt-3">Data da solicitação</label>
-                        <div class="textoinfomodal"> {{ date_format(date_create($estagio->data_solicitacao), 'd/m/Y') }}
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <label class="tituloinfomodal form-label mt-3">Professor</label>
-                        <div class="textoinfomodal"> {{ $estagio->orientador->user->name }} </div>
-                    </div>
-                    <div class="mb-3">
-                        <label class="tituloinfomodal form-label mt-3">Estudante</label>
-                        <div class="textoinfomodal"> {{ $estagio->aluno->nome_aluno }} </div>
-                    </div>
-                    <div class="mb-3">
-                        <label class="tituloinfomodal form-label mt-3">Supervisor</label>
-                        <div class="textoinfomodal"> {{ $estagio->supervisor }} </div>
-                    </div>
-                    
-                </div>
+                        <div class="mb-3">
 
-                <div class="modal-footer border-0">
+                            <label class="tituloinfomodal form-label mt-3">Tipo</label>
+                            <div class="textoinfomodal">
+                                @if ($estagio->tipo == 'eno')
+                                    {{ 'Não-Obrigatório' }}
+                                @else
+                                    {{ 'Obrigatório' }}
+                                @endif
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label class="tituloinfomodal form-label mt-3">Data de início</label>
+                            <div class="textoinfomodal"> {{ date_format(date_create($estagio->data_inicio), 'd/m/Y') }}</div>
+                        </div>
+                        <div class="mb-3">
+                            <label class="tituloinfomodal form-label mt-3">Data de fim</label>
+                            <div class="textoinfomodal"> {{ date_format(date_create($estagio->data_fim), 'd/m/Y') }}</div>
+                        </div>
+                        <div class="mb-3">
+                            <label class="tituloinfomodal form-label mt-3">Data da solicitação</label>
+                            <div class="textoinfomodal"> {{ date_format(date_create($estagio->data_solicitacao), 'd/m/Y') }}
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label class="tituloinfomodal form-label mt-3">Professor</label>
+                            <div class="textoinfomodal"> {{ $estagio->orientador->user->name }} </div>
+                        </div>
+                        <div class="mb-3">
+                            <label class="tituloinfomodal form-label mt-3">Estudante</label>
+                            <div class="textoinfomodal"> {{ $estagio->aluno->nome_aluno }} </div>
+                        </div>
+                        <div class="mb-3">
+                            <label class="tituloinfomodal form-label mt-3">Supervisor</label>
+                            <div class="textoinfomodal"> {{ $estagio->supervisor }} </div>
+                        </div>
+                        
+                    </div>
+
+                    <div class="modal-footer border-0">
+                    </div>
                 </div>
-            </div>
+            @else
+                <h3 style="margin-top: 1rem">Você não possui permissão!</h3>
+                <a class="btn btn-primary submit" style="margin-top: 1rem" href="{{ url('/login') }}">Voltar</a>
+            @endcan
         </div>
-
     </div>
-    </div>
-@else
-    <h3 style="margin-top: 1rem">Você não possui permissão!</h3>
-    <a class="btn btn-primary submit" style="margin-top: 1rem" href="{{ url('/login') }}">Voltar</a>
-@endcan
+</div>
