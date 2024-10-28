4292211c92104e039b5e2da1a3205e87a6039ee6
Rener Tomé <55724549+Rener1@users.noreply.github.com>
2024-10-28 16:47:07 -0300

    Corrigindo problemas com o bootstrap.

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -5,9 +5,6 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
-        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
-    </script>
     <script src="https://code.jquery.com/jquery-3.6.1.min.js"
         integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 </head>
