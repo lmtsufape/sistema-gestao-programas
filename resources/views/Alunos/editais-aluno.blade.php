d38ecfa14c5c97ed22c6b58aa9f29dff62f50cbb
Rener Tomé <55724549+Rener1@users.noreply.github.com>
2024-10-29 14:56:24 -0300

    Correção de erro na tela de editais de aluno.

--- resources/views/Alunos/editais-aluno.blade.php
+++ resources/views/Alunos/editais-aluno.blade.php
@@ -1,7 +1,6 @@
 @extends('templates.app')
 
 @section('body')
- 
     @if (session('sucesso'))
         <div class="alert alert-success">
             {{ session('sucesso') }}
@@ -32,8 +31,8 @@
                         <th scope="col" class="text-center align-middle">Programa</th>
                         <th class="text-center">
                             Ações
-                            <button type="button" class="infobutton align-bottom" data-bs-toggle="modal" data-bs-target="#modal_legenda"
-                                title="Ver legenda dos ícones">
+                            <button type="button" class="infobutton align-bottom" data-bs-toggle="modal"
+                                data-bs-target="#modal_legenda" title="Ver legenda dos ícones">
                                 <img src="{{ asset('images/infolegenda.svg') }}" alt="Legenda"
                                     style="height: 20px; width: 20px;">
                             </button>
@@ -41,57 +40,61 @@
                     </tr>
                 </thead>
                 @foreach ($editais as $edital)
-                    <tbody>
-                        <tr>
+                    @isset($edital)
+                        <tbody>
+                            <tr>
 
-                            <td class="align-middle">{{ $edital->titulo_edital }}</td>
-                            <td class="align-middle">{{ date_format(date_create($edital->data_inicio), 'd/m/Y') }}</td>
-                            <td class="align-middle">{{ date_format(date_create($edital->data_fim), 'd/m/Y') }}</td>
-                            <td class="align-middle">{{ $edital->programa->nome }}</td>
-                            <td class="align-middle">
+                                <td class="align-middle">{{ $edital->titulo_edital }}</td>
+                                <td class="align-middle">{{ date_format(date_create($edital->data_inicio), 'd/m/Y') }}</td>
+                                <td class="align-middle">{{ date_format(date_create($edital->data_fim), 'd/m/Y') }}</td>
+                                <td class="align-middle">{{ $edital->programa->nome }}</td>
+                                <td class="align-middle">
 
-                                <a type="button" data-bs-toggle="modal" data-bs-target="#modal_show{{ $edital->id }}">
-                                    <img src="{{ asset('images/information.svg') }}" alt="Info edital"
-                                        style="height: 30px; width: 30px;" title="Informações do Edital">
-                                </a>
-                                <a type="button" alt="Listar orientadores"
-                                    href="{{ route('edital.listar_orientadores', ['id' => $edital->id]) }}">
-                                    <img src="{{ asset('images/orientadores.svg') }}" alt="Listar orientadores"
-                                        style="height: 30px; width: 30px;" title="Professores Vinculados">
-                                </a>
-                                @if ($edital->programa->nome == 'Monitoria')
-                                    <a type="button" data-bs-toggle="modal" data-bs-target="#modal_frequencia">
-                                        <img src="{{ asset('images/add_edital.svg') }}" alt="Frequencia"
-                                            style="height: 30px; width: 30px; ">
+                                    <a type="button" data-bs-toggle="modal" data-bs-target="#modal_show{{ $edital->id }}">
+                                        <img src="{{ asset('images/information.svg') }}" alt="Info edital"
+                                            style="height: 30px; width: 30px;" title="Informações do Edital">
                                     </a>
-                                    @foreach ($pivos as $pivo)
-                                        @php
-                                            $latestFrequencia = null;
-                                        @endphp
+                                    <a type="button" alt="Listar orientadores"
+                                        href="{{ route('edital.listar_orientadores', ['id' => $edital->id]) }}">
+                                        <img src="{{ asset('images/orientadores.svg') }}" alt="Listar orientadores"
+                                            style="height: 30px; width: 30px;" title="Professores Vinculados">
+                                    </a>
+                                    @if ($edital->programa->nome == 'Monitoria')
+                                        <a type="button" data-bs-toggle="modal" data-bs-target="#modal_frequencia">
+                                            <img src="{{ asset('images/add_edital.svg') }}" alt="Frequencia"
+                                                style="height: 30px; width: 30px; ">
+                                        </a>
+                                        @foreach ($pivos as $pivo)
+                                            @php
+                                                $latestFrequencia = null;
+                                            @endphp
 
-                                        @foreach ($pivo->frequencias as $frequencia)
-                                            @if ($frequencia->frequencia_mensal != null)
-                                                @php
-                                                    $latestFrequencia = $frequencia;
-                                                @endphp
+                                            @foreach ($pivo->frequencias as $frequencia)
+                                                @if ($frequencia->frequencia_mensal != null)
+                                                    @php
+                                                        $latestFrequencia = $frequencia;
+                                                    @endphp
+                                                @endif
+                                            @endforeach
+
+                                            @if ($latestFrequencia)
+                                                <a type="button"
+                                                    href="{{ route('frequencia.download', ['fileName' => $latestFrequencia->frequencia_mensal]) }}"
+                                                    target="_blank">
+                                                    <img src="{{ asset('images/download.svg') }}" alt="baixar arquivo"
+                                                        style="height: 30px; width: 30px; ">
+                                                </a>
                                             @endif
                                         @endforeach
+                                    @endif
 
-                                        @if ($latestFrequencia)
-                                            <a type="button" href="{{ route('frequencia.download', ['fileName' => $latestFrequencia->frequencia_mensal]) }}" target="_blank" >
-                                                <img src="{{ asset('images/download.svg') }}" alt="baixar arquivo" style="height: 30px; width: 30px; ">
-                                            </a>
-                                        @endif
-                                    @endforeach
+                                </td>
+                            </tr>
 
-                                @endif
-
-                            </td>
-                        </tr>
-
-                    </tbody>
-                    @include('Edital.components.modal_show', ['edital' => $edital])
-                    @include('Alunos.components.modal_frequencia')
+                        </tbody>
+                        @include('Edital.components.modal_show', ['edital' => $edital])
+                        @include('Alunos.components.modal_frequencia')
+                    @endisset
                 @endforeach
             </table>
         </div>
