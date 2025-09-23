04f4e82b29dd89212c761b4f6401362eb85ac1bd
Rener1 <rener.tome132@gmail.com>
2025-09-22 23:34:20 -0300

    Criando tabela de jobs. Aumentando quantidade de tentativas do job. Adicionando detalhes de erros no log. O job só tenta novamente caso a falha seja na conexão, erros internos fazem o job falhar automaticamente.

--- app/Jobs/ParecerRelatorioFinalJob.php
+++ app/Jobs/ParecerRelatorioFinalJob.php
@@ -12,14 +12,16 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Carbon\Carbon;
+use Illuminate\Http\Client\RequestException;
+use Illuminate\Http\Client\ConnectionException;
 
 class ParecerRelatorioFinalJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public $tries = 3; // numero de tentativas
+    public $tries = 4; // numero de tentativas
     public $timeout = 30; // tempo limite para cada tentativa em segundos
-    public $backoff = [10, 60, 300]; // Atraso entre tentativas em segundos
+    public $backoff = [30, 60, 300]; // Atraso entre tentativas em segundos
 
     protected $relatorioId;
 
@@ -30,68 +32,67 @@ class ParecerRelatorioFinalJob implements ShouldQueue
 
     public function handle()
     {
-        // Encontra o relatório final
-        $relatorio = RelatorioFinal::findOrFail($this->relatorioId);
+        try {
+            $relatorio = RelatorioFinal::findOrFail($this->relatorioId);
 
-        // Define o período do relatório com base no semestre do edital
-        $semestre = $relatorio->editalAlunoOrientador->edital->semestre;
-        $parts = explode('.', $semestre);
-        $ano = intval($parts[0]);
-        $semestre_num = intval($parts[1]);
+            // Define o período do relatório com base no semestre do edital
+            $semestre = $relatorio->editalAlunoOrientador->edital->semestre;
+            $parts = explode('.', $semestre);
+            $ano = intval($parts[0]);
+            $semestre_num = intval($parts[1]);
 
-        if ($semestre_num == 1) {
-            $dataInicio = Carbon::createFromDate($ano, 1, 1);
-            $dataFim = Carbon::createFromDate($ano, 6, 30);
-        } else {
-            $dataInicio = Carbon::createFromDate($ano, 7, 1);
-            $dataFim = Carbon::createFromDate($ano, 12, 31);
-        }
+            if ($semestre_num == 1) {
+                $dataInicio = Carbon::createFromDate($ano, 1, 1);
+                $dataFim = Carbon::createFromDate($ano, 6, 30);
+            } else {
+                $dataInicio = Carbon::createFromDate($ano, 7, 1);
+                $dataFim = Carbon::createFromDate($ano, 12, 31);
+            }
 
-        // Monta o JSON conforme a especificação
-        $json = [
-            [
-                'titulo' => "Relatório Final - $semestre",
-                'inicio' => $dataInicio->format('Y-m-d'),
-                'fim' => $dataFim->format('Y-m-d'),
-                'natureza' => $relatorio->editalAlunoOrientador->natureza ?? 'tipo teste importação api',
-                'atividades' => [
-                    [
-                        'descricao' => $relatorio->editalAlunoOrientador->titulo,
-                        'inicio' => Carbon::parse($relatorio->editalAlunoOrientador->data_inicio)->format('Y-m-d'),
-                        'fim' => Carbon::parse($relatorio->editalAlunoOrientador->data_fim)->format('Y-m-d'),
-                        'participantes' => [
-                            [
-                                'nome' => $relatorio->editalAlunoOrientador->aluno->user->name,
-                                'email' => $relatorio->editalAlunoOrientador->aluno->user->email,
-                                'cpf' => $relatorio->editalAlunoOrientador->aluno->user->cpf,
-                                'carga' => 20, // Adjust based on actual data
-                                'curso' => $relatorio->editalAlunoOrientador->aluno->curso->nome ?? 'Ciência da Computação'
+            // Monta o JSON conforme a especificação
+            $json = [
+                [
+                    'titulo' => "Relatório Final - $semestre",
+                    'inicio' => $dataInicio->format('Y-m-d'),
+                    'fim' => $dataFim->format('Y-m-d'),
+                    'natureza' => $relatorio->editalAlunoOrientador->natureza ?? 'tipo teste importação api',
+                    'atividades' => [
+                        [
+                            'descricao' => $relatorio->editalAlunoOrientador->titulo,
+                            'inicio' => Carbon::parse($relatorio->editalAlunoOrientador->data_inicio)->format('Y-m-d'),
+                            'fim' => Carbon::parse($relatorio->editalAlunoOrientador->data_fim)->format('Y-m-d'),
+                            'participantes' => [
+                                [
+                                    'nome' => $relatorio->editalAlunoOrientador->aluno->user->name,
+                                    'email' => $relatorio->editalAlunoOrientador->aluno->user->email,
+                                    'cpf' => $relatorio->editalAlunoOrientador->aluno->user->cpf,
+                                    'carga' => 20, // Adjust based on actual data
+                                    'curso' => $relatorio->editalAlunoOrientador->aluno->curso->nome ?? 'Ciência da Computação'
+                                ]
                             ]
                         ]
                     ]
                 ]
-            ]
-        ];
+            ];
 
-        // Funcionalidade de envio para sistema externo
-        $sistema = SistemaExterno::where('name', 'Certifica')->first();
-        $url = config('services.certifica.url');
-        $endpoint = config('services.certifica.endpoint');
+            // Funcionalidade de envio para sistema externo
+            $sistema = SistemaExterno::where('name', 'Certifica')->first();
+            $url = config('services.certifica.url');
+            $endpoint = config('services.certifica.endpoint');
 
-        // Verifica se o sistema externo e o token estão configurados
-        if (!$sistema || !$sistema->api_token || !$url || !$endpoint) {
-            Log::warning('External system configuration not found or incomplete', [
-                'system' => $sistema ? $sistema->name : 'not set',
-                'url' => $url ?? 'not set',
-                'endpoint' => $endpoint ?? 'not set',
-                'api_token' => $sistema && $sistema->api_token ? 'set' : 'not set',
-            ]);
-            $this->fail();
-            return;
-        }
+            // Verifica se o sistema externo e o token estão configurados
+            if (!$sistema || !$sistema->api_token || !$url || !$endpoint) {
+                Log::warning('External system configuration not found or incomplete', [
+                    'system' => $sistema ? $sistema->name : 'not set',
+                    'url' => $url ?? 'not set',
+                    'endpoint' => $endpoint ?? 'not set',
+                    'api_token' => $sistema && $sistema->api_token ? 'set' : 'not set',
+                ]);
+                $this->fail();
+                return;
+            }
 
-        // Envia o JSON para o sistema externo e registra logs
-        try {
+            // Tenta enviar a requisição para o sistema externo e registra logs
             $response = Http::withHeaders([
                 'Authorization' => 'Bearer ' . $sistema->api_token,
                 'Accept' => 'application/json',
@@ -100,27 +101,33 @@ class ParecerRelatorioFinalJob implements ShouldQueue
 
             if ($response->successful()) {
                 $sistema->update(['last_used_at' => now()]);
-                Log::info('O JSON foi enviado com sucesso', [
+                Log::info('O JSON foi enviado com sucesso.', [
                     'system' => $sistema->name,
                     'url' => $url,
                     'response_status' => $response->status(),
                     'relatorio_id' => $this->relatorioId,
                 ]);
             } else {
-                Log::error('Falha ao enviar o JSON', [
-                    'system' => $sistema->name,
-                    'url' => $url,
-                    'response_status' => $response->status(),
-                    'response_body' => $response->body(),
-                    'relatorio_id' => $this->relatorioId,
-                ]);
-                $this->fail();
+                throw new RequestException($response);
             }
-        } catch (\Exception $e) {
-            Log::error('Falha no envio do JSON', [
+        } catch (RequestException | ConnectionException $e) {
+            // Falha na comunicação externa (i.e., erro de http ou problema na rede)
+            Log::error('Falha na comunicação com o sistema externo.', [
                 'system' => $sistema->name,
                 'url' => $url,
                 'error' => $e->getMessage(),
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+                'relatorio_id' => $this->relatorioId,
+                'attempt' => $this->attempts(),
+            ]);
+            throw $e;
+        } catch (\Exception $e) {
+            // Falha no sistema interno (i.e., banco de dados, dados inválidos)
+            Log::error('Falha interna.', [
+                'error' => $e->getMessage(),
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
                 'relatorio_id' => $this->relatorioId,
             ]);
             $this->fail();
