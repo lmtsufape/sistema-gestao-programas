36709f3b0b0bd9600347073c8c09d26133a3e185
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2025-09-29 18:44:54 -0300

    fix: remove campos de data de inicio e fim em programas

--- database/seeders/ProgramasSeeder.php
+++ database/seeders/ProgramasSeeder.php
@@ -17,36 +17,26 @@ class ProgramasSeeder extends Seeder
         $programa = Programa::create([
             'nome' => "PAVI",
             'descricao' => 'Projeto PAVI',
-            'data_inicio' =>"2023-06-21",
-            'data_fim' =>"2024-06-21"
         ]);
 
         $programa1 = Programa::create([
             'nome' => "Monitoria",
             'descricao' => 'Projeto Monitoria',
-            'data_inicio' =>"2023-06-21",
-            'data_fim' =>"2024-06-21"
         ]);
 
         $programa2 = Programa::create([
             'nome' => "Tutoria",
             'descricao' => 'Projeto Tutoria',
-            'data_inicio' =>"2023-06-21",
-            'data_fim' =>"2024-06-21"
         ]);
 
         $programa3 = Programa::create([
             'nome' => "BIA",
             'descricao' => 'Projeto BIA',
-            'data_inicio' =>"2023-06-21",
-            'data_fim' =>"2024-06-21"
         ]);
 
         $programa4 = Programa::create([
             'nome' => "PETI",
             'descricao' => 'Projeto PETI',
-            'data_inicio' =>"2023-06-21",
-            'data_fim' =>"2024-06-21"
         ]);
     }
 }
